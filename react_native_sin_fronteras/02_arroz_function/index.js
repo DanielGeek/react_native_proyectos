9917bd22480f58525fb258e193317c8482fdfbde
DanielGeek <daniel30081990@gmail.com>
2020-09-30 17:47:57 -0300

    13. Fat arrow function

---
+++ react_native_sin_fronteras/02_arroz_function/index.js
@@ -0,0 +1,14 @@
+// function suma (a,b) {
+//     return a + b;
+// }
+
+// const suma = (a, b) => {
+//     console.log(a, b);
+//     return a + b;
+// }
+
+const suma = (a, b) => a + b;
+
+const resultado = suma(1, 2);
+
+console.log(resultado);
