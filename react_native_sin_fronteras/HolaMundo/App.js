44d2eb8a5cc4e7d7d3d4ce2ed4601e4c24a3e32e
DanielGeek <daniel30081990@gmail.com>
2020-09-30 21:36:22 -0300

    18. Nuestro primer componente

--- react_native_sin_fronteras/HolaMundo/App.js
+++ react_native_sin_fronteras/HolaMundo/App.js
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Texto = () => {
+  return (
+    <Text>Hola Mundo!</Text>
+  )
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto />
       <StatusBar style="auto" />
     </View>
   );
