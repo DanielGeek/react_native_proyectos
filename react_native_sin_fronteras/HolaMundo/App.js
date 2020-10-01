93a58de3ef76454260f6545840e293c4c0f9592d
DanielGeek <daniel30081990@gmail.com>
2020-10-01 12:28:10 -0300

    27. Dimensiones con flex

--- react_native_sin_fronteras/HolaMundo/App.js
+++ react_native_sin_fronteras/HolaMundo/App.js
@@ -2,20 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const Texto = (props) => {
+const Texto = ({ style }) => {
   const [texto, setTexto] = useState("Hola mundo!")
   const actualizaTexto = () => {
     setTexto('Chao mundo!')
   }
   return (
-    <Text style={styles.text} onPress={actualizaTexto}>{texto}</Text>
+    <Text style={[styles.text, style]} onPress={actualizaTexto}>{texto}</Text>
   )
 }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto />
+      <Texto style={styles.red} />
+      <Texto style={styles.green} />
+      <Texto style={styles.blue} />
       <StatusBar style="auto" />
     </View>
   );
@@ -23,8 +25,22 @@ export default function App() {
 
 const styles = StyleSheet.create({
   text: {
-    color: 'red',
+    color: 'white',
     fontSize: 24,
+    height: 100,
+    width: 100,
+  },
+  red: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  green: {
+    flex: 2,
+    backgroundColor: 'green',
+  },
+  blue: {
+    flex: 3,
+    backgroundColor: 'blue',
   },
   container: {
     flex: 1,
