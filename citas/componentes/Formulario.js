8250958f7c50f483fe78cc3e93d7dffc593a8ab5
DanielGeek <daniel30081990@gmail.com>
2021-01-01 20:06:40 -0300

    37. Instalando una Dependencia para la Fecha

--- citas/componentes/Formulario.js
+++ citas/componentes/Formulario.js
@@ -1,8 +1,23 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 export const Formulario = () => {
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = (date) => {
+        console.warn('A date has been picked: ', date);
+        hideDatePicker();
+    };
     return (
         <>
             <View style={styles.formulario}>
@@ -26,6 +41,15 @@ export const Formulario = () => {
                         style={styles.input}
                         onChangeText={(texto) => console.log(texto)}
                         keyboardType="numeric"
+                    />
+                </View>
+                <View>
+                    <Button title="Show Date Picker" onPress={showDatePicker} />
+                    <DateTimePickerModal
+                        isVisible={isDatePickerVisible}
+                        mode="date"
+                        onConfirm={handleConfirm}
+                        onCancel={hideDatePicker}
                     />
                 </View>
                 <View>
