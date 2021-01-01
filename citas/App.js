a5e5849c045e6e0bf504fc618fafb853d184da6b
DanielGeek <daniel30081990@gmail.com>
2021-01-01 15:51:31 -0300

    29. Mostrar elementos del State en React Native

--- citas/App.js
+++ citas/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,10 +8,22 @@ import {
 
 const App = () => {
 
+  // Definir el state de citas
+  const [citas, setCitas] = useState([
+    { id: '1', paciente: 'Hook', propietario: 'Daniel', sintomas: 'No come' },
+    { id: '2', paciente: 'UseState', propietario: 'Elias', sintomas: 'No Sale' },
+    { id: '3', paciente: 'Reducer', propietario: 'Jessica', sintomas: 'No Guarda' },
+  ]);
   return (
     <>
       <View style={styles.contenedor}>
         <Text style={styles.titulo}>Administrador de Citas</Text>
+
+        {citas.map(cita => (
+          <View>
+            <Text key={cita.id}>{cita.paciente}</Text>
+          </View>
+        ))}
       </View>
     </>
   );
