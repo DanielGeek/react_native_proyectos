bdb3954c0f2a840a1ae902f56b44b0d7482c58cc
DanielGeek <daniel30081990@gmail.com>
2021-01-04 00:43:08 -0300

    160. Finalizando el Sidebar y routing

--- restaurant_app/restaurant-cliente/src/App.js
+++ restaurant_app/restaurant-cliente/src/App.js
@@ -7,14 +7,16 @@ import { Sidebar } from "./components/ui/Sidebar";
 
 function App() {
   return (
-    <div>
+    <div className="md:flex min-h-screen">
       <Sidebar />
 
-      <Routes>
-        <Route path="/" element={<Ordenes />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
-      </Routes>
+      <div className="md:w-3/5 xl:w-4/5">
+        <Routes>
+          <Route path="/" element={<Ordenes />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- restaurant_app/restaurant-cliente/src/components/ui/Sidebar.js
+++ restaurant_app/restaurant-cliente/src/components/ui/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Sidebar = () => {
     return (
         <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
             <div className="p-6">
                 <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">RestaurantApp</p>
+                <p className="mt-3 text-gray-600">Administra tu restaurant en las siguientes opciones:</p>
+
+                <nav className="mt-10">
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" end to="/">Ordenes</NavLink>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" end to="/menu">Menú</NavLink>
+                </nav>
             </div>
         </div>
     )
