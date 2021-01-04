d1028afdc9f7654bc0daa2f43fae6b27592749a0
DanielGeek <daniel30081990@gmail.com>
2021-01-04 00:23:01 -0300

    159. Estilos al Proyecto

--- restaurant_app/restaurant-cliente/src/App.js
+++ restaurant_app/restaurant-cliente/src/App.js
@@ -3,10 +3,13 @@ import { Route, Routes } from "react-router";
 import { Ordenes } from "./components/paginas/Ordenes";
 import { Menu } from "./components/paginas/Menu";
 import { NuevoPlatillo } from "./components/paginas/NuevoPlatillo";
+import { Sidebar } from "./components/ui/Sidebar";
 
 function App() {
   return (
     <div>
+      <Sidebar />
+
       <Routes>
         <Route path="/" element={<Ordenes />} />
         <Route path="/menu" element={<Menu />} />
---
+++ restaurant_app/restaurant-cliente/src/components/ui/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Sidebar = () => {
+    return (
+        <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
+            <div className="p-6">
+                <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">RestaurantApp</p>
+            </div>
+        </div>
+    )
+}
