6fb078fc363883a9b9801af0f4634c38033096e3
DanielGeek <daniel30081990@gmail.com>
2021-01-05 21:31:03 -0300

    172. Acceder a los datos

--- restaurant_app/restaurant-cliente/src/components/paginas/Menu.js
+++ restaurant_app/restaurant-cliente/src/components/paginas/Menu.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { FirebaseContext } from '../../firebase';
+
 
 export const Menu = () => {
+
+    const { firebase } = useContext(FirebaseContext);
+
+    useEffect(() => {
+        const obtenerPlatillos = () => {
+            const resultado = firebase.db.collection('productos').onSnapshot(handleSnapshot);
+
+            resultado.forEach(platillo => {
+                console.log(platillo.data());
+            });
+        }
+        obtenerPlatillos();
+    }, [firebase.db]);
+
+    // Snapshot nos permite utilizar la base de datos en tiempo real de firestore
+    const handleSnapshot = (snapshot) => {
+
+    }
+
     return (
         <>
             <h1 className="text-3xl font-light mb-4">Menú</h1>
