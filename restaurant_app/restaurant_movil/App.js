c1ab0e34a19ab4d13491c0c26cc8c8b977c80933
DanielGeek <daniel30081990@gmail.com>
2021-01-16 00:03:28 -0300

    180. Agregando las diferentes vistas

--- restaurant_app/restaurant_movil/App.js
+++ restaurant_app/restaurant_movil/App.js
@@ -5,6 +5,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import NuevaOrden from './views/NuevaOrden';
+import Menu from './views/Menu';
+import DetallePlatillo from './views/DetallePlatillo';
+import FormularioPlatillo from './views/FormularioPlatillo';
+import ResumenPedido from './views/ResumenPedido';
+import ProgresoPedido from './views/ProgresoPedido';
 
 const Stack = createStackNavigator();
 
@@ -12,12 +17,61 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#FFDA00'
+            },
+            headerTitleStyle: {
+              fontWeight: 'bold'
+            }
+          }}
+        >
           <Stack.Screen
             name="NuevaOrden"
             component={NuevaOrden}
             options={{
               title: "Nueva Orden"
+            }}
+          />
+
+          <Stack.Screen
+            name="Menu"
+            component={Menu}
+            options={{
+              title: "Nuestro Menú"
+            }}
+          />
+
+          <Stack.Screen
+            name="DetallePlatillo"
+            component={DetallePlatillo}
+            options={{
+              title: "Detalle Platillo"
+            }}
+          />
+
+          <Stack.Screen
+            name="FormularioPlatillo"
+            component={FormularioPlatillo}
+            options={{
+              title: "Ordenar Platillo"
+            }}
+          />
+
+          <Stack.Screen
+            name="ResumenPedido"
+            component={ResumenPedido}
+            options={{
+              title: "Resumen Pedido"
+            }}
+          />
+
+          <Stack.Screen
+            name="ProgresoPedido"
+            component={ProgresoPedido}
+            options={{
+              title: "Progreso de Pedido"
             }}
           />
         </Stack.Navigator>
