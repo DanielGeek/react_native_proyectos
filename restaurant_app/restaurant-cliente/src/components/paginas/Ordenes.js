a2cdbe2905b912daa2a482077da01e242e00f27c
DanielGeek <daniel30081990@gmail.com>
2021-01-21 03:21:44 -0300

    208. Consulta para obtener las ordenes que aún no estén completas React Web

--- restaurant_app/restaurant-cliente/src/components/paginas/Ordenes.js
+++ restaurant_app/restaurant-cliente/src/components/paginas/Ordenes.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { FirebaseContext } from '../../firebase';
 
 export const Ordenes = () => {
+
+    // context con las operaciones de firebase
+    const { firebase } = useContext(FirebaseContext);
+
+    // State con las ordenes
+    const [ordenes, guardarOrdenes] = useState([]);
+
+    useEffect(() => {
+        const obtenerOrdenes = () => {
+            // onSnapshot es para obtener resultado en tiempo real
+            firebase.db.collection('ordenes').where('completado', '==', false).onSnapshot(manejarSnapshot);
+        }
+        obtenerOrdenes();
+    }, []);
+
+    function manejarSnapshot(snapshot) {
+        const ordenes = snapshot.docs.map(doc => {
+            return {
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+
+        guardarOrdenes(ordenes);
+    }
+
     return (
         <>
             <h1 className="text-3xl font-light mb-4">Ordenes</h1>
