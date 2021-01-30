f6f3bce3161b9b1dd4c27e36b2d9361cd1522727
DanielGeek <daniel30081990@gmail.com>
2021-01-30 01:33:33 -0300

    219. Creando un Query y un Resolver

--- 04-upTask/upTaskGraphQL/index.js
+++ 04-upTask/upTaskGraphQL/index.js
@@ -2,14 +2,43 @@ const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
 
-  type Query {
+  type Curso {
+    titulo: String
+    tecnologia: String
+  }
 
+  type Query {
+    obtenerCursos : [Curso]
   }
 
 `;
 
+const cursos = [
+  {
+    titulo: "Javascript Moderno Guía definitiva Construye +10 Proyectos",
+    tecnologia: "Javascript ES6",
+  },
+  {
+    titulo: "React - La Guía Completa: Hooks Context Redux MERN +15 Apps",
+    tecnologia: "React",
+  },
+  {
+    titulo: "Node js - Bootcamp Desarrollo Web inc. MVC y REST API",
+    tecnologia: "React",
+  },
+  {
+    titulo: "React js - ReactJS Avanzado - FullStack React GraphQL y Apollo",
+    tecnologia: "React",
+  },
+];
 
-const server = new ApolloServer();
+const resolvers = {
+  Query: {
+    obtenerCursos: () => cursos
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Server listo en la URL ${url}`);
