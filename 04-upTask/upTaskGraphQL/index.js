01f754afd2b3b6d0c72a259d938b48e0ff5da78c
DanielGeek <daniel30081990@gmail.com>
2021-01-31 00:49:57 -0300

    233. Almacenando el Usuario Autenticado en el Proyecto

--- 04-upTask/upTaskGraphQL/index.js
+++ 04-upTask/upTaskGraphQL/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server');
-
+const jwt = require('jsonwebtoken');
+require('dotenv').config('variables.env');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 
@@ -8,7 +9,29 @@ const conectarDB = require('./config/db');
 // Conectar a la BD
 conectarDB();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  /* -------------------------------------------------------------------------- */
+  /*                         obtener token de los header                        */
+  /* -------------------------------------------------------------------------- */
+  context: ({ req }) => {
+    const token = req.headers['authorization'] || '';
+    if (token) {
+      try {
+        /* -------------------------------------------------------------------------- */
+        /*      si el token es correcto obtengo los datos del usuario autenticado     */
+        /* -------------------------------------------------------------------------- */
+        const usuario = jwt.verify(token, process.env.SECRETA);
+        return {
+          usuario
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+});
 
 server.listen().then(({ url }) => {
   console.log(`Server listo en la URL ${url}`);
