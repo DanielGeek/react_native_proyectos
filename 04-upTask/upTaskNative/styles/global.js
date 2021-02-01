1e9f284f28875c4c49778751b01794316a4adb78
DanielGeek <daniel30081990@gmail.com>
2021-01-31 23:03:54 -0300

    245. Estilos al Formulario de Autenticación

---
+++ 04-upTask/upTaskNative/styles/global.js
@@ -0,0 +1,43 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+  },
+  // centrar verticalmente
+  contenido: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginHorizontal: '2.5%',
+    flex: 1,
+  },
+  titulo: {
+    textAlign: 'center',
+    marginBottom: 20,
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    marginBottom: 20,
+  },
+  boton: {
+    backgroundColor: '#28303B',
+  },
+  botonTexto: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  enlace: {
+    color: '#FFF',
+    marginTop: 40,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
+    textTransform: 'uppercase',
+  },
+});
+
+export default globalStyles;
--- 04-upTask/upTaskNative/views/Login.js
+++ 04-upTask/upTaskNative/views/Login.js
@@ -10,26 +10,27 @@ import {
   Item,
   Toast,
 } from 'native-base';
+import globalStyles from '../styles/global';
 
 const Login = () => {
   return (
-    <Container>
-      <View>
-        <H1>UpTask</H1>
+    <Container style={[globalStyles.contenedor, {backgroundColor: '#e84347'}]}>
+      <View style={globalStyles.contenido}>
+        <H1 style={globalStyles.titulo}>UpTask</H1>
 
         <Form>
-          <Item inlineLabel last>
+          <Item inlineLabel last style={globalStyles.input}>
             <Input placeholder="Email" />
           </Item>
-          <Item inlineLabel last>
+          <Item inlineLabel last style={globalStyles.input}>
             <Input secureTextEntry={true} placeholder="Password" />
           </Item>
         </Form>
 
-        <Button squeare block>
-          <Text>Iniciar Sesión</Text>
+        <Button squeare block style={globalStyles.boton}>
+          <Text style={globalStyles.botonTexto}>Iniciar Sesión</Text>
         </Button>
-        <Text>Crear Cuenta</Text>
+        <Text style={globalStyles.enlace}>Crear Cuenta</Text>
       </View>
     </Container>
   );
