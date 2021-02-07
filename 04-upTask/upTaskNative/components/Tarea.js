89fafa55b747515b30b642318b11715790f4b90f
DanielGeek <daniel30081990@gmail.com>
2021-02-07 01:33:54 -0300

    271. Eliminar una Tarea

--- 04-upTask/upTaskNative/components/Tarea.js
+++ 04-upTask/upTaskNative/components/Tarea.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Alert} from 'react-native';
 import {Text, ListItem, Left, Right, Icon, Toast} from 'native-base';
 import {gql, useMutation} from '@apollo/client';
 
 const ACTUALIZAR_TAREA = gql`
-  mutation actualizarTarea($id: ID!, $input: TareaInput, $estado: Boolean ) {
+  mutation actualizarTarea($id: ID!, $input: TareaInput, $estado: Boolean) {
     actualizarTarea(id: $id, input: $input, estado: $estado) {
       nombre
       id
@@ -14,10 +14,16 @@ const ACTUALIZAR_TAREA = gql`
   }
 `;
 
-const Tarea = ({tarea}) => {
+const ELIMINAR_TAREA = gql`
+  mutation eliminarTarea($id: ID!) {
+    eliminarTarea(id: $id)
+  }
+`;
 
+const Tarea = ({tarea}) => {
   // Apollo
   const [actualizarTarea] = useMutation(ACTUALIZAR_TAREA);
+  const [eliminarTarea] = useMutation(ELIMINAR_TAREA);
 
   // Cambia el estado de una tarea a completo o incompleto
   const cambiarEstado = async () => {
@@ -41,9 +47,41 @@ const Tarea = ({tarea}) => {
     }
   };
 
+  // Dialogo para eliminar o no una tarea
+  const mostrarEliminar = () => {
+    Alert.alert('Eliminar Tarea', '¿Deseas eliminar esta tarea?', [
+      {
+        text: 'Cancelar',
+        style: 'cancel',
+      },
+      {
+        text: 'Confirmar',
+        onPress: () => eliminarTareaDB(),
+      },
+    ]);
+  };
+
+  // Eliminar tarea de la base de datos
+  const eliminarTareaDB = async () => {
+    const {id} = tarea;
+
+    try {
+      const {data} = await eliminarTarea({
+        variables: {
+          id,
+        },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <ListItem onPress={() => cambiarEstado()}>
+      <ListItem
+        onPress={() => cambiarEstado()}
+        onLongPress={() => mostrarEliminar()}>
         <Left>
           <Text>{tarea.nombre}</Text>
         </Left>
