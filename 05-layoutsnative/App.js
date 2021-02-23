135db3f68fc995d85218b4e89a492b73f8f9b428
DanielGeek <daniel30081990@gmail.com>
2021-02-22 23:57:53 -0300

    50. Factor de Crecimiento con flex

--- 05-layoutsnative/App.js
+++ 05-layoutsnative/App.js
@@ -18,22 +18,27 @@ const styles = StyleSheet.create({
   contenedor: {
     backgroundColor: 'cornflowerblue',
     flex: 1,
+    flexDirection: 'column',
   },
   caja1: {
     padding: 20,
     backgroundColor: 'navy',
+    flex: 1,
   },
   caja2: {
     padding: 20,
     backgroundColor: 'yellow',
+    flex: 1,
   },
   caja3: {
     padding: 20,
     backgroundColor: 'green',
+    flex: 1,
   },
   caja4: {
     padding: 20,
     backgroundColor: 'teal',
+    flex: 3,
   },
 });
 
