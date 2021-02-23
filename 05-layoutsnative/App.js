bf07e3ba4998af09ce6111cebb6ba50cf3ff56b9
DanielGeek <daniel30081990@gmail.com>
2021-02-22 23:50:07 -0300

    49. Las diferentes direcciones: Flex column o row

--- 05-layoutsnative/App.js
+++ 05-layoutsnative/App.js
@@ -4,11 +4,37 @@ import {View, StyleSheet} from 'react-native';
 const App = () => {
   return (
     <>
-      
+      <View style={styles.contenedor}>
+        <View style={styles.caja1}></View>
+        <View style={styles.caja2}></View>
+        <View style={styles.caja3}></View>
+        <View style={styles.caja4}></View>
+      </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: 'cornflowerblue',
+    flex: 1,
+  },
+  caja1: {
+    padding: 20,
+    backgroundColor: 'navy',
+  },
+  caja2: {
+    padding: 20,
+    backgroundColor: 'yellow',
+  },
+  caja3: {
+    padding: 20,
+    backgroundColor: 'green',
+  },
+  caja4: {
+    padding: 20,
+    backgroundColor: 'teal',
+  },
+});
 
 export default App;
