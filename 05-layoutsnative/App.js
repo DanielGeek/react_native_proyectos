5f5c9753e2b317ed6d56c6e5ba8883e44d5ce051
DanielGeek <daniel30081990@gmail.com>
2021-02-23 00:06:04 -0300

    51. Alineando contenido

--- 05-layoutsnative/App.js
+++ 05-layoutsnative/App.js
@@ -19,26 +19,23 @@ const styles = StyleSheet.create({
     backgroundColor: 'cornflowerblue',
     flex: 1,
     flexDirection: 'column',
+    alignItems: 'center',
   },
   caja1: {
     padding: 20,
     backgroundColor: 'navy',
-    flex: 1,
   },
   caja2: {
     padding: 20,
     backgroundColor: 'yellow',
-    flex: 1,
   },
   caja3: {
     padding: 20,
     backgroundColor: 'green',
-    flex: 1,
   },
   caja4: {
     padding: 20,
     backgroundColor: 'teal',
-    flex: 3,
   },
 });
 
