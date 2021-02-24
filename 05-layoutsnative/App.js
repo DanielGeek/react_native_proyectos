b2526f222a73a87e96e6aa55d2516817bbc2bcf7
DanielGeek <daniel30081990@gmail.com>
2021-02-23 22:33:59 -0300

    55. Creando un ScrollView Horizontal

--- 05-layoutsnative/App.js
+++ 05-layoutsnative/App.js
@@ -1,15 +1,51 @@
 import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import { View, StyleSheet, Image, Text, ScrollView } from 'react-native';
 
 const App = () => {
   return (
     <>
       <View>
-        <View style={{flexDirection: 'row'}}>
+        <View style={{ flexDirection: 'row' }}>
           <Image
             style={styles.banner}
             source={require('./assets/img/bg.jpg')}
           />
+        </View>
+
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Qué hacer en Paris</Text>
+          <ScrollView horizontal>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad1.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad2.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad3.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad4.jpg')}
+              />
+            </View>
+            <View>
+              <Image
+                style={styles.ciudad}
+                source={require('./assets/img/actividad5.jpg')}
+              />
+            </View>
+          </ScrollView>
         </View>
       </View>
     </>
@@ -20,6 +56,19 @@ const styles = StyleSheet.create({
   banner: {
     height: 250,
     flex: 1,
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width: 250,
+    height: 300,
+    marginRight: 10,
   },
 });
 
