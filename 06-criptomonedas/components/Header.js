1c47a6a92fc968a054a204af2691300383a02c48
DanielGeek <daniel30081990@gmail.com>
2021-02-25 00:11:50 -0300

    63. Finalizando el Header

--- 06-criptomonedas/components/Header.js
+++ 06-criptomonedas/components/Header.js
@@ -8,6 +8,13 @@ const styles = StyleSheet.create({
   encabezado: {
     paddingTop: Platform.OS === 'ios' ? 50 : 10,
     fontFamily: 'Lato-Black',
+    backgroundColor: '#5E49E2',
+    paddingBottom: 10,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontSize: 20,
+    color: '#FFF',
+    marginBottom: 30,
   },
 });
 
