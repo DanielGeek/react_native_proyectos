8586bc9d69ccdaa7244936fc3f810bddb0d197b9
DanielGeek <daniel30081990@gmail.com>
2021-02-25 00:08:33 -0300

    62. Agregar una fuente personalizada

---
+++ 06-criptomonedas/components/Header.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text, StyleSheet, Platform } from 'react-native';
+
+const Header = () => <Text style={styles.encabezado}>Criptomonedas</Text>;
+
+const styles = StyleSheet.create({
+  encabezado: {
+    paddingTop: Platform.OS === 'ios' ? 50 : 10,
+    fontFamily: 'Lato-Black',
+  },
+});
+
+export default Header;
