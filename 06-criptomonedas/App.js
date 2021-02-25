b49a3b98bd043a7a82b376b3db6fdb0e2651beca
DanielGeek <daniel30081990@gmail.com>
2021-02-25 00:18:16 -0300

    64. Mostrando una imagen de criptomonedas

--- 06-criptomonedas/App.js
+++ 06-criptomonedas/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Image
+        style={styles.imagen}
+        source={require('./assets/img/cryptomonedas.png')}
+      />
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%',
+  },
 });
 
 export default App;
