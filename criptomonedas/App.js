731234bdbcf2ee32c47ce843c336bcee3f714f80
DanielGeek <daniel30081990@gmail.com>
2021-02-24 23:29:41 -0300

    60. Creando el Proyecto y primeros pasos

---
+++ criptomonedas/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
