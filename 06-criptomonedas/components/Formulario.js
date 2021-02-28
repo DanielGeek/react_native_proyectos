111fee42430a1c18306ef54da2b7207b66cef890
DanielGeek <daniel30081990@gmail.com>
2021-02-28 05:56:47 -0300

    66. Agregando un Picker para diferentes opciones

--- 06-criptomonedas/components/Formulario.js
+++ 06-criptomonedas/components/Formulario.js
@@ -1,12 +1,18 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import { Picker } from '@react-native-community/picker';
 
 const Formulario = () => {
   return (
     <View>
       <Text style={styles.label}>Moneda</Text>
-
+      <Picker>
+        <Picker.Item label="- Seleccione -" value="" />
+        <Picker.Item label="Dolar de Estados Unidos" value="USD" />
+        <Picker.Item label="Peso Mexicano" value="MXN" />
+        <Picker.Item label="Libra Esterlina" value="GBP" />
+      </Picker>
       <Text style={styles.label}>Criptomoneda</Text>
     </View>
   );
