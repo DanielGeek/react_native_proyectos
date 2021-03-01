667cf202990e2c6ad927694c130f752f7093fb43
DanielGeek <daniel30081990@gmail.com>
2021-02-28 23:57:06 -0300

    79. Creando el Proyecto y la API que utilizaremos

---
+++ 07-clima_app/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
