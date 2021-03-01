dbe5c6480b2eb7c9ef0a2730a0cb6977aa4826e8
DanielGeek <daniel30081990@gmail.com>
2021-02-28 23:09:44 -0300

    74. Agregando más información a la cotización

--- 06-criptomonedas/components/Cotizacion.js
+++ 06-criptomonedas/components/Cotizacion.js
@@ -7,8 +7,39 @@ const Cotizacion = ({resultado}) => {
   if (Object.keys(resultado).length === 0 ) {return null;}
 
   return (
-    <Text>El precio de la Criptodivisa es: {resultado.PRICE}</Text>
+    <View style={styles.resultado}>
+      <Text style={styles.texto}>
+        <Text style={styles.span}>{resultado.PRICE}</Text>
+      </Text>
+      <Text style={styles.texto}>Precio más alto del día: {' '}
+        <Text style={styles.span}>{resultado.HIGHDAY}</Text>
+      </Text>
+      <Text style={styles.texto}>Precio más bajo del día: {' '}
+        <Text style={styles.span}>{resultado.LOWDAY}</Text>
+      </Text>
+      <Text style={styles.texto}>Variación últimas 24 horas: {' '}
+        <Text style={styles.span}>{resultado.CHANGEPCT24HOUR} % </Text>
+      </Text>
+      <Text style={styles.texto}>Última Actualización: {' '}
+        <Text style={styles.span}>{resultado.LASTUPDATE}</Text>
+      </Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  resultado: {
+
+  },
+  text: {
+
+  },
+  precio: {
+
+  },
+  span: {
+
+  },
+});
 
 export default Cotizacion;
