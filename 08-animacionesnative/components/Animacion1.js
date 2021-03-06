9c736ef4cf1f61cedef93aa7bfcd283422b3b1db
DanielGeek <daniel30081990@gmail.com>
2021-03-06 18:06:51 -0300

    98. .timing

--- 08-animacionesnative/components/Animacion1.js
+++ 08-animacionesnative/components/Animacion1.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import {Text} from 'react-native';
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react';
+import {Animated, StyleSheet, Text} from 'react-native';
 
 const Animacion1 = () => {
+
+  const [ animacion ] = useState( new Animated.Value(0) );
+
+  useEffect(() => {
+    Animated.timing(
+      animacion, {
+        toValue: 1, // valor al que llega
+        duration: 500, // cantidad de tiempo en llegar
+      }
+    ).start();
+  }, [animacion]);
+
   return (
-    <Text>Animación 1</Text>
+    <Animated.View
+      style={{
+        opacity: animacion,
+      }}
+    >
+      <Text style={styles.texto}>Animación 1</Text>
+    </Animated.View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  texto: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
 
 export default Animacion1;
