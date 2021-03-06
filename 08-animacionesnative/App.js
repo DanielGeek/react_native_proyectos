9ed20cb81c18257017c1ee72414a5b193b1ab0a4
DanielGeek <daniel30081990@gmail.com>
2021-03-06 19:19:07 -0300

    100. Animación de Textos

--- 08-animacionesnative/App.js
+++ 08-animacionesnative/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-import Animacion1 from './components/Animacion1';
-import Animacion2 from './components/Animacion2';
+import {StyleSheet, View} from 'react-native';
+// import Animacion1 from './components/Animacion1';
+// import Animacion2 from './components/Animacion2';
+import Animacion3 from './components/Animacion3';
 
 const App = () => {
   return (
     <>
       <View style={styles.contenido}>
         {/* <Animacion1 /> */}
-        <Animacion2 />
+        {/* <Animacion2 /> */}
+        <Animacion3 />
       </View>
     </>
   );
---
+++ 08-animacionesnative/components/Animacion3.js
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import React, { useEffect, useState } from 'react';
+import {Animated, StyleSheet, View} from 'react-native';
+
+const Animacion3 = () => {
+
+  const [ animacion ] = useState( new Animated.Value(14) );
+
+  useEffect(() => {
+    Animated.timing(
+      animacion, {
+        toValue: 40, // valor al que llega
+        duration: 500, // cantidad de tiempo en llegar
+        useNativeDriver: false,
+      }
+    ).start();
+  }, []);
+
+  return (
+    <View>
+      <Animated.Text
+        style={[styles.texto, {fontSize: animacion }]}
+        >Animación 3</Animated.Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  texto: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
+
+export default Animacion3;
