7707d1b927464d7aad1a2455d0c1c1629627665c
DanielGeek <daniel30081990@gmail.com>
2021-03-11 23:11:44 -0300

    122. Headers Dinamicos

--- 10-react_navigation/App.js
+++ 10-react_navigation/App.js
@@ -16,8 +16,18 @@ const App = () => {
     <>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Inicio">
-          <Stack.Screen name="Inicio" component={Inicio} />
-          <Stack.Screen name="Nosotros" component={Nosotros} />
+          <Stack.Screen
+            name="Inicio"
+            component={Inicio}
+            options={{title: 'Componente Principal'}}
+          />
+          <Stack.Screen
+            name="Nosotros"
+            component={Nosotros}
+            options={({route}) => ({
+              title: route.params.clientId,
+            })}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
