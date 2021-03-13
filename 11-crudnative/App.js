ba2f6076207cf1ead659d325ba61f434e6fc019e
DanielGeek <daniel30081990@gmail.com>
2021-03-12 22:53:34 -0300

    126. Primeros pasos con el nuevo Proyecto

---
+++ 11-crudnative/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
