eaaecf713579d560c5d2ee7f79a0911d6efe009e
DanielGeek <daniel30081990@gmail.com>
2021-03-14 18:32:54 -0300

    131. Instalando un framework UI

--- 11-crudnative/App.js
+++ 11-crudnative/App.js
@@ -17,14 +17,36 @@ import Inicio from './views/Inicio';
 import NuevoCliente from './views/NuevoCliente';
 import DetallesCliente from './views/DetallesCliente';
 
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
 const Stack = createStackNavigator();
+
+// Definir el tema
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774F2',
+    accent: '#0655BF',
+  },
+};
+
 
 const App = () => {
   return (
     <>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="NuevoCliente"
+          initialRouteName="Inicio"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.colors.primary,
+            },
+            headerTintColor: theme.colors.surface,
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
         >
           <Stack.Screen
             name="Inicio"
