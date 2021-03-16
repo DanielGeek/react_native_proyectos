e714f30752e5c7f9bc70d50a6385c16bcf5ea28b
DanielGeek <daniel30081990@gmail.com>
2021-03-16 00:04:55 -0300

    135. Agregando el Resto de campos al formulario

--- 11-crudnative/views/NuevoCliente.js
+++ 11-crudnative/views/NuevoCliente.js
@@ -5,12 +5,47 @@ import { TextInput, Headline, Button } from 'react-native-paper';
 import globalStyles from '../styles/global';
 
 const NuevoCliente = () => {
+
+  const leerNombre = () => {
+    console.log('escribiendo...');
+  };
+
   return (
     <View style={globalStyles.contenedor}>
         <Headline style={globalStyles.titulo}>Añadir Nuevo Cliente</Headline>
-        <TextInput />
+        <TextInput
+          label="Nombre"
+          placeholder="Daniel"
+          onChangeText={ () => leerNombre() }
+          style={styles.input}
+        />
+        <TextInput
+          label="Teléfono"
+          placeholder="123123123"
+          onChangeText={ () => leerNombre() }
+          style={styles.input}
+        />
+        <TextInput
+          label="Correo"
+          placeholder="correo@gmal.com"
+          onChangeText={ () => leerNombre() }
+          style={styles.input}
+        />
+        <TextInput
+          label="Empresa"
+          placeholder="Nombre Empresa"
+          onChangeText={ () => leerNombre() }
+          style={styles.input}
+        />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 20,
+    backgroundColor: 'transparent',
+  },
+});
 
 export default NuevoCliente;
