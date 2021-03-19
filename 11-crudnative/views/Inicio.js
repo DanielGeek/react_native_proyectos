286a1b61263f7b4c8bcfc6b7d8dc9674f9788495
DanielGeek <daniel30081990@gmail.com>
2021-03-18 23:14:36 -0300

    142. Consultar la API para obtener clientes

--- 11-crudnative/views/Inicio.js
+++ 11-crudnative/views/Inicio.js
@@ -1,8 +1,26 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from 'react-native';
+import axios from 'axios';
 
 const Inicio = () => {
+
+  // state de la app
+  const [clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    const obtenerClientesApi = async () => {
+      try {
+        const resultado = await axios.get('http://10.0.2.2:3000/clientes');
+        console.log(resultado)
+        setClientes(resultado.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerClientesApi();
+  }, []);
+
   return <Text>Desde Inicio</Text>;
 };
 
