3473f15b8747950b494e3223115bbc56e183623a
DanielGeek <daniel30081990@gmail.com>
2021-03-18 23:31:36 -0300

    144. Header Condicional

--- 11-crudnative/views/Inicio.js
+++ 11-crudnative/views/Inicio.js
@@ -2,7 +2,8 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, View} from 'react-native';
 import axios from 'axios';
-import {List} from 'react-native-paper';
+import {List, Headline} from 'react-native-paper';
+import globalStyles from '../styles/global';
 
 const Inicio = () => {
 
@@ -23,7 +24,10 @@ const Inicio = () => {
   }, []);
 
   return (
-  <View>
+  <View style={globalStyles.contenedor}>
+
+    <Headline style={globalStyles.titulo}>{clientes.length ? 'Clientes' : 'Aún no hay Clientes'}</Headline>
+
     <FlatList
       data={clientes}
       keyExtractor={cliente => (cliente.id).toString()}
