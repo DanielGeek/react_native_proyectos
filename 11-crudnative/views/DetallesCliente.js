c48b0d48dd2d93c4d2e0f912314c9d1f30570eac
DanielGeek <daniel30081990@gmail.com>
2021-03-20 23:49:11 -0300

    147. Mostrando la información del Cliente

--- 11-crudnative/views/DetallesCliente.js
+++ 11-crudnative/views/DetallesCliente.js
@@ -1,12 +1,29 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Headline, Text, Subheading, Button } from 'react-native-paper';
+import globalStyles from '../styles/global';
 
 const DetallesCliente = ({route}) => {
-  console.log(route.params);
+  const {nombre, telefono, correo, empresa } = route.params.item;
+
   return (
-    <Text>Desde Detalle Cliente</Text>
+    <View style={globalStyles.contenedor}>
+      <Headline style={globalStyles.titulo}>{nombre}</Headline>
+      <Text style={styles.texto}>Empresa: <Subheading> {empresa}</Subheading></Text>
+      <Text style={styles.texto}>Correo: <Subheading> {correo}</Subheading></Text>
+      <Text style={styles.texto}>Teléfono: <Subheading> {telefono}</Subheading></Text>
+
+      <Button>Eliminar Cliente</Button>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  texto: {
+    marginBottom: 20,
+    fontSize: 18,
+  },
+});
 
 export default DetallesCliente;
