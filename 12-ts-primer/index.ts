4e6dfde42555e891b322b6c4aef2a620dec5783a
DanielGeek <daniel30081990@gmail.com>
2021-03-29 00:59:01 -0300

    6. Interfaces & Types

--- 12-ts-primer/index.ts
+++ 12-ts-primer/index.ts
@@ -1,19 +1,47 @@
-let loading = false;
-let x:number = 6;
-let color = "red";
+// let loading = false;
+// let x:number = 6;
+// let color = "red";
 
-let list: (number | string )[] = [1, 2, 3, "string"];
-let list2: Array<number> = [1, 2, 3];
+// let list: (number | string )[] = [1, 2, 3, "string"];
+// let list2: Array<number> = [1, 2, 3];
 
-let tuple: [string, number];
+// let tuple: [string, number];
 
-tuple = ["121212", 22];
+// tuple = ["121212", 22];
 
-let value:any = 2;
-(value as number).toFixed();
+// let value:any = 2;
+// (value as number).toFixed();
 
-function add(x: number, y: number): number {
-  return x + y;
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
+
+// add(1, 2);
+
+type Shape = {
+  color: string;
+  size?: number;
 }
 
-add(1, 2);
+function newShape(options: Shape): void {
+  console.log(options.color);
+  console.log(options.size?.toFixed());
+}
+
+newShape({color: "123"});
+
+type TriangleTypes = "equilateral" | "isosceles" | "right-angled";
+
+type Triangle = {
+  type: TriangleTypes;
+  onCreate: (type: TriangleTypes) => void;
+} & Shape;
+
+function newTriangle(options: Triangle) {
+  console.log(options);
+  options.onCreate(options.type);
+}
+
+newTriangle({color: "red", type: "equilateral", onCreate: (type) => {
+  console.log(type === "equilateral")
+}})
