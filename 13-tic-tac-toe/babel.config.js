eefae212be82487784457e2799c9a55898a7b17c
DanielGeek <daniel30081990@gmail.com>
2021-04-03 00:01:36 -0300

    17. Installing and Setting Up a Module Resolver

--- 13-tic-tac-toe/babel.config.js
+++ 13-tic-tac-toe/babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ['babel-preset-expo']
+        presets: ['babel-preset-expo'],
+        plugins: [
+            ["module-resolver", {
+                alias: {
+                    "@screens": "./src/screens",
+                    "@components": "./src/components",
+                    "@utils": "./src/utils",
+                    "@contexts": "./src/contexts",
+                    "@config": "./src/config",
+                    "@assets": "./assets"
+                }
+            }]
+        ]
     };
 };
