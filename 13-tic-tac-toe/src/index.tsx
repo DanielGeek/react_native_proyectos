0acf8141686d4d2453a790f239821dcb07789936
DanielGeek <daniel30081990@gmail.com>
2021-04-03 04:37:10 -0300

    21. Setting Up our Navigation

--- 13-tic-tac-toe/src/index.tsx
+++ 13-tic-tac-toe/src/index.tsx
@@ -1,29 +1,11 @@
 import React, { ReactElement } from "react";
-import { StyleSheet, View } from "react-native";
-import { Text, AppBootstrap } from "@components";
+import { AppBootstrap } from "@components";
+import Navigator from "@config/navigator";
 
 export default function App(): ReactElement {
     return (
         <AppBootstrap>
-            <View style={styles.container}>
-                <Text
-                    onPress={() => {
-                        alert(true);
-                    }}
-                    style={{ fontSize: 25 }}
-                >
-                    Hello World <Text weight="400">sasas</Text>
-                </Text>
-            </View>
+            <Navigator />
         </AppBootstrap>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
-});
