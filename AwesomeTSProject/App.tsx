552100e8a7705e40319b14252fa2a84ff27d5e16
DanielGeek <daniel30081990@gmail.com>
2021-04-21 01:43:03 -0400

    34. Windows: Crear proyecto de React Native

---
+++ AwesomeTSProject/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  )
+}
+
+export default App;
