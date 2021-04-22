2a3bd2bb62a08862eff5b00f8a369c216c661ab2
DanielGeek <daniel30081990@gmail.com>
2021-04-22 00:19:28 -0400

    51. Crear un contador

--- 14-AwesomeTSProject/App.tsx
+++ 14-AwesomeTSProject/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
+// import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
+import { ContadorScreen } from './src/screens/ContadorScreen';
 
 export const App = () => {
   return (
-    <HolaMundoScreen />
+    // <HolaMundoScreen />
+    <ContadorScreen />
   );
 };
