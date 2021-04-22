bfabc0a23cb30c19b05cee26710754a9f4c4040f
DanielGeek <daniel30081990@gmail.com>
2021-04-22 00:40:48 -0400

    54. StyleSheet

--- 14-AwesomeTSProject/src/screens/ContadorScreen.tsx
+++ 14-AwesomeTSProject/src/screens/ContadorScreen.tsx
@@ -1,36 +1,39 @@
 import React, { useState } from 'react'
-import { Text, View, Button, Touchable, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 export const ContadorScreen = () => {
 
   const [contador, setContador] = useState(10);
 
   return (
-      <View style={{
-          flex: 1,
-          justifyContent: 'center'
-        }}>
-        <Text
-            style={{
-              textAlign: 'center',
-              fontSize: 40,
-              top: -15
-            }}
-        >
+      <View style={styles.container}>
+        <Text style={styles.title}>
           Contador: { contador }
         </Text>
 
         <TouchableOpacity
           onPress={ () => setContador( contador + 1)}
         >
-          <View style={{
-            backgroundColor:'red',
-            borderRadius: 100
-          }}
-          >
+          <View style={styles.bottonIncrementar}>
             <Text>+1</Text>
           </View>
         </TouchableOpacity>
       </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15
+  },
+  bottonIncrementar: {
+    backgroundColor:'red',
+    borderRadius: 100
+  }
+})
