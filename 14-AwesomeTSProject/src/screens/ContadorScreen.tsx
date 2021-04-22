8b2ae4fce1fe3f2989082267f5ea189841a609f5
DanielGeek <daniel30081990@gmail.com>
2021-04-22 00:59:01 -0400

    55. Botón personalizado flotante

--- 14-AwesomeTSProject/src/screens/ContadorScreen.tsx
+++ 14-AwesomeTSProject/src/screens/ContadorScreen.tsx
@@ -12,10 +12,20 @@ export const ContadorScreen = () => {
         </Text>
 
         <TouchableOpacity
+          style={ styles.fabLocationBR }
           onPress={ () => setContador( contador + 1)}
         >
-          <View style={styles.bottonIncrementar}>
-            <Text>+1</Text>
+          <View style={styles.fab}>
+            <Text style={ styles.fabText }> +1 </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={ styles.fabLocationBL }
+          onPress={ () => setContador( contador - 1)}
+        >
+          <View style={styles.fab}>
+            <Text style={ styles.fabText }> -1 </Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -28,12 +38,31 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   title: {
-    textAlign: 'center',
     fontSize: 40,
-    top: -15
+    top: -15,
+    textAlign: 'center',
   },
-  bottonIncrementar: {
-    backgroundColor:'red',
-    borderRadius: 100
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25
+  },
+  fabLocationBL:{
+    position: 'absolute',
+    bottom: 25,
+    left: 25
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center'
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center'
   }
 })
