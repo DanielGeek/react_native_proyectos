8816db02f14669bcf93997ada04e70f5a0e18efe
DanielGeek <daniel30081990@gmail.com>
2021-04-21 23:50:00 -0400

    49. Hola Mundo

--- 14-AwesomeTSProject/App.tsx
+++ 14-AwesomeTSProject/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Text, View} from 'react-native';
 
 export const App = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
+    <View
+      style={{
+        flex: 1,
+        // backgroundColor: 'red',
+        justifyContent: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 45,
+          textAlign: 'center'
+        }}>
+        Hola Mundo!!!!
+      </Text>
     </View>
-  )
-}
+  );
+};
 
 export default App;
