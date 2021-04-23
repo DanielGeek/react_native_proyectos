b99e7b5531d9436e1c66757206a0e7af0c07b80f
DanielGeek <daniel30081990@gmail.com>
2021-04-22 23:53:14 -0400

    56. Componente personalizado: FAB - Floating Action Button

--- 14-AwesomeTSProject/src/screens/ContadorScreen.tsx
+++ 14-AwesomeTSProject/src/screens/ContadorScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Fab } from '../components/Fab';
 
 export const ContadorScreen = () => {
 
@@ -11,23 +12,18 @@ export const ContadorScreen = () => {
           Contador: { contador }
         </Text>
 
-        <TouchableOpacity
-          style={ styles.fabLocationBR }
-          onPress={ () => setContador( contador + 1)}
-        >
-          <View style={styles.fab}>
-            <Text style={ styles.fabText }> +1 </Text>
-          </View>
-        </TouchableOpacity>
+        <Fab
+          title="+1"
+        />
 
-        <TouchableOpacity
+        {/* <TouchableOpacity
           style={ styles.fabLocationBL }
           onPress={ () => setContador( contador - 1)}
         >
           <View style={styles.fab}>
             <Text style={ styles.fabText }> -1 </Text>
           </View>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
   )
 }
@@ -42,27 +38,4 @@ const styles = StyleSheet.create({
     top: -15,
     textAlign: 'center',
   },
-  fabLocationBR: {
-    position: 'absolute',
-    bottom: 25,
-    right: 25
-  },
-  fabLocationBL:{
-    position: 'absolute',
-    bottom: 25,
-    left: 25
-  },
-  fab: {
-    backgroundColor: '#5856D6',
-    width: 60,
-    height: 60,
-    borderRadius: 100,
-    justifyContent: 'center'
-  },
-  fabText: {
-    color: 'white',
-    fontSize: 25,
-    fontWeight: 'bold',
-    alignSelf: 'center'
-  }
 })
