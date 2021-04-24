0a62523ffe6e272bd252c9c2608bd8df302c7af5
DanielGeek <daniel30081990@gmail.com>
2021-04-24 01:09:46 -0400

    65. Continuación de proyecto - Diseños y Flexbox

--- 14-contadorApp/App.tsx
+++ 14-contadorApp/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {SafeAreaView} from 'react-native';
 // import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
-import {ContadorScreen} from './src/screens/ContadorScreen';
+// import {ContadorScreen} from './src/screens/ContadorScreen';
+import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 
 export const App = () => {
   return (
-    // <HolaMundoScreen />
-    <ContadorScreen />
+    <SafeAreaView>
+      {/* <HolaMundoScreen />
+      <ContadorScreen /> */}
+      <BoxObjectModelScreen />
+    </SafeAreaView>
   );
 };
---
+++ 14-contadorApp/src/screens/BoxObjectModelScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const BoxObjectModelScreen = () => {
+  return (
+    <View>
+      <Text>Box Object Model</Text>
+    </View>
+  );
+};
