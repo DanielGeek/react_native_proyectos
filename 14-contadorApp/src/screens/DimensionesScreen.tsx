0b4d28f604acd315e186ab452d849218e3102b24
DanielGeek <daniel30081990@gmail.com>
2021-04-25 19:45:06 -0400

    67. Height, Width porcentual y dimensiones de la pantalla

---
+++ 14-contadorApp/src/screens/DimensionesScreen.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {View, StyleSheet, Text, useWindowDimensions} from 'react-native';
+
+// const {width, height} = Dimensions.get('window');
+
+export const DimensionesScreen = () => {
+  const {width, height} = useWindowDimensions();
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <View
+          style={{
+            ...styles.cajaMorada,
+            width: width * 0.6,
+          }}
+        />
+        <View style={styles.cajaNaranja} />
+      </View>
+      <Text style={styles.title}>
+        W: {width}, H: {height}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 200,
+    backgroundColor: 'red',
+  },
+  cajaMorada: {
+    backgroundColor: '#5856D6',
+    width: '100%',
+    height: '50%',
+  },
+  cajaNaranja: {
+    backgroundColor: '#F0A23B',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
