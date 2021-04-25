b35097b5df353dfdb074f4e6791dfb2c2df91c2d
DanielGeek <daniel30081990@gmail.com>
2021-04-24 23:06:03 -0400

    66. Padding, Margin, Border, Width y Height

--- 14-contadorApp/src/screens/BoxObjectModelScreen.tsx
+++ 14-contadorApp/src/screens/BoxObjectModelScreen.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
-      <Text>Box Object Model</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Box Object Model</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    paddingHorizontal: 100,
+    paddingVertical: 20,
+    fontSize: 20,
+    marginHorizontal: 20,
+    // width: 250,
+    borderWidth: 10,
+    // backgroundColor: 'red',
+  },
+});
