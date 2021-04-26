4de36f2f91b7a32c23b853fd5a286a117f68c094
DanielGeek <daniel30081990@gmail.com>
2021-04-26 01:43:50 -0400

    78. Preparar nuevo componente para la tarea

---
+++ 14-contadorApp/src/screens/TareaScreen.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+export const TareaScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cajaMorada} />
+      <View style={styles.cajaNaranja} />
+      <View style={styles.cajaAzul} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#28425B',
+  },
+  cajaMorada: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#5856D6',
+  },
+  cajaNaranja: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#F0A23B',
+  },
+  cajaAzul: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#28C4D9',
+  },
+});
