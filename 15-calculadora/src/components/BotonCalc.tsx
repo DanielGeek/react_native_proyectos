5f0e2efe7f47425c29795b6d24638a8c8fe1e1b5
DanielGeek <daniel30081990@gmail.com>
2021-05-01 16:48:55 -0400

    89. Terminar todos los botones y el estilo de la calculadora

--- 15-calculadora/src/components/BotonCalc.tsx
+++ 15-calculadora/src/components/BotonCalc.tsx
@@ -5,12 +5,19 @@ import {styles} from '../theme/appTheme';
 interface Props {
   texto: string;
   color?: string;
+  ancho?: boolean;
 }
 
-export const BotonCalc = ({texto, color = '#2D2D2D'}: Props) => {
+export const BotonCalc = ({texto, color = '#2D2D2D', ancho = false}: Props) => {
   return (
-    <View style={{...styles.boton, backgroundColor: color}}>
-      <Text style={styles.botonTexto}>{texto}</Text>
+    <View style={{...styles.boton, backgroundColor: color, width: ( ancho ) ? 180 : 80 }}>
+      <Text
+        style={{
+          ...styles.botonTexto,
+          color: color === '#9B9B9B' ? 'black' : 'white',
+        }}>
+        {texto}
+      </Text>
     </View>
   );
 };
