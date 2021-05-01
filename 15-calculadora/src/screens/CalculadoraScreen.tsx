9d5ef694b7107d7ccfdf676aa0f48861778d1766
DanielGeek <daniel30081990@gmail.com>
2021-05-01 02:54:45 -0400

    87. Textos y mi primer botón

--- 15-calculadora/src/screens/CalculadoraScreen.tsx
+++ 15-calculadora/src/screens/CalculadoraScreen.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {styles} from '../theme/appTheme';
+import { styles } from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
   return (
-    <View>
-      <Text style={styles.texto}>CalculadoraScreen</Text>
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoPequeno}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
+      <View>
+        <View style={styles.boton}>
+          <Text style={styles.botonTexto}>1</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- 15-calculadora/src/theme/appTheme.tsx
+++ 15-calculadora/src/theme/appTheme.tsx
@@ -5,8 +5,33 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
   },
-  texto: {
+  calculadoraContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'flex-end',
+  },
+  resultado: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  resultadoPequeno: {
+    color: 'rgba(255,255,255, 0.5)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  boton: {
+    height: 80,
+    width: 80,
+    backgroundColor: '#9B9B9B',
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  botonTexto: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: '300',
   },
 });
