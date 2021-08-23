958d19574e8b23f6643a10351fe4fee3468dcc7a
DanielGeek <daniel30081990@gmail.com>
2021-08-22 22:58:16 -0400

    34. Implementing the getBestMove Function with the Minimax Algorithm part 1

---
+++ 13-tic-tac-toe/src/utils/player.ts
@@ -0,0 +1,30 @@
+import { BoardState } from "./types";
+import { isTerminal, getAvailableMoves, printFormattedBoard } from "./board";
+
+export const getBestMove = (state: BoardState, maximizing: boolean, depth = 0): number => {
+
+  const terminalObject = isTerminal(state);
+  if(terminalObject) {
+    if(terminalObject.winner === "x") {
+      return 100 - depth;
+    } else if(terminalObject.winner === "o") {
+      return -100 + depth;
+    }
+    return 0;
+  }
+
+  if(maximizing) {
+    let best = -100;
+    getAvailableMoves(state).forEach(index => {
+      const child: BoardState = [...state];
+      child[index] = "x";
+      console.log(`Child board (x turn) (depth: ${depth})`);
+      printFormattedBoard(child);
+      const childValue = getBestMove(child, false, depth + 1);
+      console.log("childValue", childValue);
+      best = Math.max(best, childValue);
+    })
+    console.log("best", best);
+    return best;
+  }
+};
