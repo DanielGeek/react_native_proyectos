84307a8fee85ec39a640a6bd68243d9991312066
DanielGeek <daniel30081990@gmail.com>
2021-08-28 23:57:19 -0400

    37. Integrating the getBestMove Function in our UI

--- 13-tic-tac-toe/src/utils/player.ts
+++ 13-tic-tac-toe/src/utils/player.ts
@@ -1,5 +1,5 @@
 import { BoardState } from "./types";
-import { isTerminal, getAvailableMoves, printFormattedBoard } from "./board";
+import { isTerminal, getAvailableMoves } from "./board";
 
 export const getBestMove = (state: BoardState, maximizing: boolean, depth = 0, maxDepth = -1): number => {
 
@@ -23,17 +23,12 @@ export const getBestMove = (state: BoardState, maximizing: boolean, depth = 0, m
       getAvailableMoves(state).forEach(index => {
         const child: BoardState = [...state];
         child[index] = "x";
-        console.log(`Child board (x turn) (depth: ${depth})`);
-        printFormattedBoard(child);
         const childValue = getBestMoveRecursive(child, true, depth + 1, maxDepth);
-        console.log("childValue", childValue);
         best = Math.max(best, childValue);
         if(depth === 0) {
           childValues[childValue] = childValues[childValue] ? `${childValues[childValue]}, ${index}` : `${index}`;
         }
       })
-      console.log("best", best);
-      console.log("childValues", childValues);
       if( depth === 0) {
         const arr = childValues[best].split(",");
         const rand = Math.floor(Math.random() * arr.length);
@@ -45,17 +40,12 @@ export const getBestMove = (state: BoardState, maximizing: boolean, depth = 0, m
       getAvailableMoves(state).forEach(index => {
         const child: BoardState = [...state];
         child[index] = "o";
-        console.log(`Child board (x turn) (depth: ${depth})`);
-        printFormattedBoard(child);
         const childValue = getBestMoveRecursive(child, false, depth + 1, maxDepth);
-        console.log("childValue", childValue);
         best = Math.min(best, childValue);
         if(depth === 0) {
           childValues[childValue] = childValues[childValue] ? `${childValues[childValue]}, ${index}` : `${index}`;
         }
       })
-      console.log("best", best);
-      console.log("childValues", childValues);
       if( depth === 0) {
         const arr = childValues[best].split(",");
         const rand = Math.floor(Math.random() * arr.length);
