97e36dd78089714525703ad8a6e626ac64e10f50
DanielGeek <daniel30081990@gmail.com>
2021-09-14 23:58:12 -0300

    40. Refactoring our Sounds Code into a Custom Hook part 1

--- 13-tic-tac-toe/src/screens/single-player-game/single-player-game.tsx
+++ 13-tic-tac-toe/src/screens/single-player-game/single-player-game.tsx
@@ -1,12 +1,9 @@
-import React, { ReactElement, useEffect, useState, useRef } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native'
 import { GradientBackground } from '@components';
 import styles from "./single-player-game.styles";
 import { Board } from '@components';
-import { isEmpty, BoardState, isTerminal, getBestMove, Cell } from "@utils";
-import { Audio } from 'expo-av';
-import { Haptics } from 'expo';
-// import * as Haptics from 'expo-haptics';
+import { isEmpty, BoardState, isTerminal, getBestMove, Cell, useSounds } from "@utils";
 
 export default function Game(): ReactElement {
   // prettier-ignore
@@ -17,11 +14,7 @@ export default function Game(): ReactElement {
   ]);
   const [turn, setTurn] = useState<"HUMAN" | "BOT">(Math.random() < 0.5 ? "HUMAN" : "BOT");
   const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
-  const popSoundRef = useRef<Audio.Sound | null>(null);
-  const pop2SoundRef = useRef<Audio.Sound | null>(null);
-  const winSoundRef = useRef<Audio.Sound | null>(null);
-  const lossSoundRef = useRef<Audio.Sound | null>(null);
-  const drawSoundRef = useRef<Audio.Sound | null>(null);
+  const playSound = useSounds();
 
   const gameResult = isTerminal(state);
 
@@ -32,9 +25,8 @@ export default function Game(): ReactElement {
     setstate(stateCopy);
     try {
       symbol === "x"
-          ? popSoundRef.current?.replayAsync()
-          : pop2SoundRef.current?.replayAsync();
-      // Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+          ? playSound("pop1")
+          : playSound("pop2");
     } catch (error) {
       console.log(error);
     }
@@ -67,7 +59,7 @@ export default function Game(): ReactElement {
           } catch (error) {
             console.log(error);
           }
-          alert("You Won");
+          alert("You Won!");
         }
         if(winner === "BOT") {
           try {
@@ -103,41 +95,6 @@ export default function Game(): ReactElement {
         }
       }
   }, [state, turn]);
-
-  useEffect(() => {
-    // load sounds
-    const popSoundObject = new Audio.Sound();
-    const pop2SoundObject = new Audio.Sound();
-    const winSoundObject = new Audio.Sound();
-    const lossSoundObject = new Audio.Sound();
-    const drawSoundObject = new Audio.Sound();
-
-    const loadSounds = async () => {
-      await popSoundObject.loadAsync(require("@assets/pop_1.wav"));
-      popSoundRef.current = popSoundObject;
-
-      await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
-      pop2SoundRef.current = pop2SoundObject;
-
-      await winSoundObject.loadAsync(require("@assets/win.mp3"));
-      winSoundRef.current = winSoundObject;
-
-      await lossSoundObject.loadAsync(require("@assets/loss.mp3"));
-      lossSoundRef.current = lossSoundObject;
-
-      await drawSoundObject.loadAsync(require("@assets/draw.mp3"));
-      drawSoundRef.current = drawSoundObject;
-    };
-    loadSounds();
-    return () => {
-      // unload sounds
-      popSoundObject && popSoundObject.unloadAsync();
-      pop2SoundObject && pop2SoundObject.unloadAsync();
-      winSoundObject && winSoundObject.unloadAsync();
-      lossSoundObject && lossSoundObject.unloadAsync();
-      drawSoundObject && drawSoundObject.unloadAsync();
-    }
-  }, []);
 
   return (
     <GradientBackground>
