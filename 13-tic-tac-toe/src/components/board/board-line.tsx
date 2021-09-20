d0518b13f7953411c8c353a016cf52c250ff04c6
DanielGeek <daniel30081990@gmail.com>
2021-09-20 00:59:35 -0300

    41. Creating a Component for Drawing the Winning Line

--- 13-tic-tac-toe/src/components/board/board-line.tsx
+++ 13-tic-tac-toe/src/components/board/board-line.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import { View, Text } from 'react-native';
+import { BoardResult } from '@utils';
 
-export default function BoardLine() {
+type BoardLineProps = {
+  size: number;
+  gameResult?: BoardResult | false;
+}
+
+export default function BoardLine({size, gameResult}: BoardLineProps): ReactElement {
   return (
-    <View>
-      <Text style={{ color: "#fff"}}>BoardLine</Text>
-    </View>
+    <>
+      {gameResult && gameResult.column && gameResult.direction === "V" && <View></View>}
+      {gameResult && gameResult.row && gameResult.direction === "H" && <View></View>}
+      {gameResult && gameResult.diagonal && gameResult.direction === "D" && <View></View>}
+    </>
   )
 }
