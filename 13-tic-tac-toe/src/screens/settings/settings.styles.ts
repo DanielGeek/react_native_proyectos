83dc1496a5c75cdad53cd09821cb62f47acc1175
DanielGeek <daniel30081990@gmail.com>
2021-10-04 23:58:29 -0300

    48. Creating the UI For the Settings Page

--- 13-tic-tac-toe/src/screens/settings/settings.styles.ts
+++ 13-tic-tac-toe/src/screens/settings/settings.styles.ts
@@ -1,7 +1,10 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 40
+  }
 });
 
 export default styles;
--- 13-tic-tac-toe/src/screens/settings/settings.tsx
+++ 13-tic-tac-toe/src/screens/settings/settings.tsx
@@ -7,7 +7,7 @@ export default function Settings(): ReactElement {
   return (
     <GradientBackground>
       <ScrollView contentContainerStyle={styles.container}>
-        <Text>Settings</Text>
+        <Text style={{color: "white"}}>Settings</Text>
       </ScrollView>
     </GradientBackground>
   )
