59fda2f3d9464f526afe4b59669477f8aad8342c
DangelGlobant <daniel30081990@gmail.com>
2022-04-21 23:29:48 -0400

    17. Hook - useReducer

--- 16-reinforcement/src/components/Login.tsx
+++ 16-reinforcement/src/components/Login.tsx
@@ -1,5 +1,29 @@
+import { useReducer } from "react"
+
+interface AuthState {
+  validating: boolean;
+  token: string | null;
+  username: string;
+  firstname: string;
+}
+
+const initialState: AuthState = {
+  validating: true,
+  token: null,
+  username: '',
+  firstname: ''
+}
+
+type AuthAction = { type: 'logout' };
+
+const authReducer = ( state: AuthState, action: AuthAction ): AuthState => {
+
+}
 
 export const Login = () => {
+
+  const [state, dispatch] = useReducer(authReducer, initialState)
+
   return (
     <>
       <h3>Login</h3>
