2077a4cdfb5f0406dd16e8f71442201a3769a5d9
DangelGlobant <daniel30081990@gmail.com>
2022-04-21 20:03:21 -0400

    11. Basic Types - TypeScript

--- 16-reinforcement/src/typescript/BasicTypes.tsx
+++ 16-reinforcement/src/typescript/BasicTypes.tsx
@@ -1,8 +1,18 @@
 
 export const BasicTypes = () => {
+
+  let firstName: string = 'Daniel';
+  const age: number = 31;
+  const isActive: boolean = false;
+
+  const powers: string[] = [];
+
   return (
     <>
       <h3>Basic Types</h3>
+      { firstName }, { age }, { (isActive) ? 'Active' : 'Inactive' }
+      <br />
+      { powers.join(', ')}
     </>
   )
 }
