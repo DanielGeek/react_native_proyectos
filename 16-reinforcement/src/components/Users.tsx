50b9d6be9bfedf50a72fe36493c633398ff78817
DangelGlobant <daniel30081990@gmail.com>
2022-04-23 23:31:04 -0400

    21. Set the type of HTTP responses

--- 16-reinforcement/src/components/Users.tsx
+++ 16-reinforcement/src/components/Users.tsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { reqResApi } from '../api/reqRes';
+import { ReqResList, User } from '../interfaces/reqRes';
 
 export const Users = () => {
 
+  const [users, setUsers] = useState<User[]>([]);
+
   useEffect(() => {
     // call API
-    reqResApi.get('/users')
+    reqResApi.get<ReqResList>('/users')
       .then( resp => {
-        console.log(resp.data.data);
+        console.log( resp.data.data );
       })
       .catch(console.log);
   }, [])
