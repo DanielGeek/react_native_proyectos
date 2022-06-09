75da57983f4da93f2b6898ce5295c8d432328ced
daniel-barreto <daniel30081990@gmail.com>
2022-06-09 16:37:48 -0400

    52. Create a counter

--- 17-AwesomeTSProject/App.tsx
+++ 17-AwesomeTSProject/App.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+// import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+import { ContadorScreen } from './src/screens/ContadorScreen';
 
 export const App = () => {
   return (
-    <HolaMundoScreen />
+    // <HolaMundoScreen />
+    <ContadorScreen />
   );
 };
---
+++ 17-AwesomeTSProject/src/screens/ContadorScreen.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState } from 'react';
+import { Text, View, Button } from 'react-native';
+
+export const ContadorScreen = () => {
+
+  const [contador, setContador] = useState(10);
+
+  return (
+    <View style={{
+      flex:1,
+      justifyContent: 'center',
+    }}>
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 40,
+          top: -15,
+        }}
+      >
+        Contador: { contador }
+      </Text>
+
+      <Button
+        title="Click"
+        onPress={ () => setContador( contador + 1) }
+      />
+    </View>
+  );
+};
