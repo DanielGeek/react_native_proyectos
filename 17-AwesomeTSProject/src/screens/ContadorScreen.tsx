9a77063ba71d24e96c4114f0b065880c09a46322
daniel-barreto <daniel30081990@gmail.com>
2022-06-09 18:12:22 -0400

    56. Floating Custom Button

--- 17-AwesomeTSProject/src/screens/ContadorScreen.tsx
+++ 17-AwesomeTSProject/src/screens/ContadorScreen.tsx
@@ -13,10 +13,20 @@ export const ContadorScreen = () => {
       </Text>
 
       <TouchableOpacity
+        style={ styles.fabLocationBR }
         onPress={() => setContador( contador + 1)}
       >
-        <View style={ styles.bottonIncrementar }>
-          <Text>+1</Text>
+        <View style={ styles.fab }>
+          <Text style={ styles.fabText }>+1</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={ styles.fabLocationBL }
+        onPress={() => setContador( contador - 1)}
+      >
+        <View style={ styles.fab }>
+          <Text style={ styles.fabText }>-1</Text>
         </View>
       </TouchableOpacity>
     </View>
@@ -33,8 +43,27 @@ const styles = StyleSheet.create({
     fontSize: 40,
     top: -15,
   },
-  bottonIncrementar: {
-    backgroundColor: 'red',
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
     borderRadius: 100,
-  }
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
 });
