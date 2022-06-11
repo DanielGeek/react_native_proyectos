f3a21fedf411a8ae5a7facd1bbd371ff0318cb26
daniel-barreto <daniel30081990@gmail.com>
2022-06-10 21:04:40 -0400

    58. Send optional functions and properties

--- 17-AwesomeTSProject/src/screens/ContadorScreen.tsx
+++ 17-AwesomeTSProject/src/screens/ContadorScreen.tsx
@@ -15,6 +15,13 @@ export const ContadorScreen = () => {
 
       <Fab
         title="+1"
+        onPress={() => setContador( contador + 1)}
+      />
+
+      <Fab
+        title="-1"
+        position="bl"
+        onPress={() => setContador( contador - 1)}
       />
 
       {/* <TouchableOpacity
