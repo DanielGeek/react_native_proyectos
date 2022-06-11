da6336dade74bae3c2361c19bf23b84876ffe187
daniel-barreto <daniel30081990@gmail.com>
2022-06-10 21:23:03 -0400

    59. Conditional Style

--- 17-AwesomeTSProject/src/components/Fab.tsx
+++ 17-AwesomeTSProject/src/components/Fab.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native';
 
 interface Props {
   title: string;
@@ -10,26 +10,33 @@ interface Props {
 export const Fab = ({ title, onPress, position = 'br' }: Props ) => {
 
   return (
-    <TouchableOpacity
-      style={ styles.fabLocationBR }
-      onPress={ onPress }
+    <View
+      style={[
+        styles.fabLocation,
+        ( position === 'bl' ) ? styles.left : styles.right,
+      ]}
     >
-      <View style={ styles.fab }>
-        <Text style={ styles.fabText }>{ title }</Text>
-      </View>
-    </TouchableOpacity>
+      <TouchableNativeFeedback
+          onPress={ onPress }
+          background={ TouchableNativeFeedback.Ripple('#28425B', false, 30) }
+      >
+        <View style={ styles.fab }>
+          <Text style={ styles.fabText }>{ title }</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  fabLocationBR: {
+  fabLocation: {
     position: 'absolute',
     bottom: 25,
+  },
+  right: {
     right: 25,
   },
-  fabLocationBL: {
-    position: 'absolute',
-    bottom: 25,
+  left: {
     left: 25,
   },
   fab: {
@@ -38,6 +45,15 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 100,
     justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 4.65,
+
+    elevation: 8,
   },
   fabText: {
     color: 'white',
