87691cc3b370f1cd3e8f99863d89d847741c4425
daniel-barreto <daniel30081990@gmail.com>
2022-06-10 22:00:52 -0400

    66. Project Continuation - Layouts and Flexbox

--- 17-AwesomeTSProject/App.tsx
+++ 17-AwesomeTSProject/App.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
-import { ContadorScreen } from './src/screens/ContadorScreen';
+// import { ContadorScreen } from './src/screens/ContadorScreen';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 
 export const App = () => {
   return (
-    // <HolaMundoScreen />
-    <ContadorScreen />
+    <SafeAreaView>
+      {/* <HolaMundoScreen />
+      <ContadorScreen /> */}
+      <BoxObjectModelScreen />
+    </SafeAreaView>
   );
 };
