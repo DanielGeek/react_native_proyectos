af8b1a77feda28b56fd558575204150ad5d9bfec
daniel-barreto <daniel30081990@gmail.com>
2022-06-12 19:09:30 -0400

    71. Absolute position

--- 17-AwesomeTSProject/src/screens/PositionScreen.tsx
+++ 17-AwesomeTSProject/src/screens/PositionScreen.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native';
 export const PositionScreen = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.cajaVerde} />
       <View style={styles.cajaMorada} />
       <View style={styles.cajaNaranja} />
     </View>
@@ -13,8 +14,10 @@ export const PositionScreen = () => {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
+      // width: 400,
+      // height: 400,
+      // justifyContent: 'center',
+      // alignItems: 'center',
       backgroundColor: '#28C4D9',
     },
     cajaMorada: {
@@ -23,6 +26,9 @@ const styles = StyleSheet.create({
       backgroundColor: '#5856D6',
       borderWidth: 10,
       borderColor: 'white',
+      position: 'absolute',
+      top: 0,
+      right: 0,
     },
     cajaNaranja: {
       width: 100,
@@ -30,6 +36,21 @@ const styles = StyleSheet.create({
       backgroundColor: '#F0A23B',
       borderWidth: 10,
       borderColor: 'white',
-      top: 10,
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+    },
+    cajaVerde: {
+      // width: 100,
+      // height: 100,
+      backgroundColor: 'green',
+      borderWidth: 10,
+      borderColor: 'white',
+      // position: 'absolute',
+      // bottom: 0,
+      // left: 0,
+      // top: 0,
+      // right: 0,
+      ...StyleSheet.absoluteFillObject,
     },
 });
