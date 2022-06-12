895d0b3dce8ef1e3334b58971391f1f01fa2709d
daniel-barreto <daniel30081990@gmail.com>
2022-06-11 22:04:32 -0400

    68. Height, Width percentage and screen dimensions

--- 17-AwesomeTSProject/App.tsx
+++ 17-AwesomeTSProject/App.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
-import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+// import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+import { DimensionesScreen } from './src/screens/DimensionesScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {/* <HolaMundoScreen />
       <ContadorScreen /> */}
-      <BoxObjectModelScreen />
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionesScreen />
     </SafeAreaView>
   );
 };
