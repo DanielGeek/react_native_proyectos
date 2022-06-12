6569c242a5b6edd72d3194458dd0763a3d1d297a
daniel-barreto <daniel30081990@gmail.com>
2022-06-11 21:35:02 -0400

    67. Padding, Margin, Border, Width and Height

--- 17-AwesomeTSProject/App.tsx
+++ 17-AwesomeTSProject/App.tsx
@@ -7,7 +7,7 @@ import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 
 export const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       {/* <HolaMundoScreen />
       <ContadorScreen /> */}
       <BoxObjectModelScreen />
--- 17-AwesomeTSProject/src/screens/BoxObjectModelScreen.tsx
+++ 17-AwesomeTSProject/src/screens/BoxObjectModelScreen.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export const BoxObjectModelScreen = () => {
   return (
-    <View>
-      <Text>Box Object Model</Text>
+    <View style={ styles.container }>
+      <Text style={ styles.title }>Box Object Model</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    paddingHorizontal: 100,
+    paddingVertical: 20,
+    fontSize: 20,
+    marginHorizontal: 20,
+    // width: 250,
+    borderWidth: 10,
+  },
+});
