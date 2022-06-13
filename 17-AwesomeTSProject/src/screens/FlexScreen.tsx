a55df2005f7672be0184cb4abcd34688b33782b2
daniel-barreto <daniel30081990@gmail.com>
2022-06-13 14:57:53 -0400

    74. Flex Direction

--- 17-AwesomeTSProject/src/screens/FlexScreen.tsx
+++ 17-AwesomeTSProject/src/screens/FlexScreen.tsx
@@ -15,21 +15,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
+    flexDirection: 'row',
   },
   caja1: {
-    flex: 3,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   caja2: {
-    flex: 2,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   caja3: {
-    flex: 1,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
