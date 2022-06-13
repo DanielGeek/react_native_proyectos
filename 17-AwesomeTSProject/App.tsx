e18d3c3001726cc849084c8629362db963c8bfda
daniel-barreto <daniel30081990@gmail.com>
2022-06-13 17:31:53 -0400

    79. Prepare new component for task

--- 17-AwesomeTSProject/App.tsx
+++ 17-AwesomeTSProject/App.tsx
@@ -6,17 +6,19 @@ import { SafeAreaView } from 'react-native';
 // import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 // import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { PositionScreen } from './src/screens/PositionScreen';
-import { FlexScreen } from './src/screens/FlexScreen';
+// import { FlexScreen } from './src/screens/FlexScreen';
+import { TareaScreen } from './src/screens/TareaScreen';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#28425B' }}>
       {/* <HolaMundoScreen />
       <ContadorScreen /> */}
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen /> */}
       {/* <PositionScreen /> */}
-      <FlexScreen />
+      {/* <FlexScreen /> */}
+      <TareaScreen />
     </SafeAreaView>
   );
 };
