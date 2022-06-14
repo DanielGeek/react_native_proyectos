8f4dcc66f80f78be48ecda734cd53d74b17c04ca
daniel-barreto <daniel30081990@gmail.com>
2022-06-13 21:25:24 -0400

    89. Calculator button and its rows

--- 18-calculadora-2022/src/screens/CalculadoraScreen.tsx
+++ 18-calculadora-2022/src/screens/CalculadoraScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import { BotonCalc } from '../components/BotonCalc';
 import { styles } from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
@@ -8,10 +9,11 @@ export const CalculadoraScreen = () => {
       <Text style={styles.resultadoPequeno}>1,500.00</Text>
       <Text style={styles.resultado}>1,500.00</Text>
 
-      <View>
-        <View style={styles.boton}>
-          <Text style={styles.botonTexto}>1</Text>
-        </View>
+      <View style={styles.fila}>
+        <BotonCalc texto="C" color="#9B9B9B" />
+        <BotonCalc texto="+/-" color="#9B9B9B" />
+        <BotonCalc texto="del" color="#9B9B9B" />
+        <BotonCalc texto="/" color="#FF9427" />
       </View>
 
     </View>
