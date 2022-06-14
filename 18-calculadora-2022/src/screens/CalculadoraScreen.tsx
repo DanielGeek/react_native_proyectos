8dd67b7f54a01fe1223a13f0215075dd0b89e5b8
daniel-barreto <daniel30081990@gmail.com>
2022-06-13 20:49:30 -0400

    88. Texts and my first button

--- 18-calculadora-2022/src/screens/CalculadoraScreen.tsx
+++ 18-calculadora-2022/src/screens/CalculadoraScreen.tsx
@@ -4,8 +4,16 @@ import { styles } from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
   return (
-    <View>
-      <Text style={styles.texto}>CalculadoraScreen</Text>
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoPequeno}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
+
+      <View>
+        <View style={styles.boton}>
+          <Text style={styles.botonTexto}>1</Text>
+        </View>
+      </View>
+
     </View>
   );
 };
