aa00c3c4d3d74cdf874daff5a3717dd3fa5dacc1
daniel-barreto <daniel30081990@gmail.com>
2022-06-15 20:56:08 -0400

    96. Custom Hook: useCalculadora

--- 18-calculadora-2022/src/screens/CalculadoraScreen.tsx
+++ 18-calculadora-2022/src/screens/CalculadoraScreen.tsx
@@ -2,125 +2,24 @@ import React, { useRef, useState } from 'react';
 import {Text, View} from 'react-native';
 import { BotonCalc } from '../components/BotonCalc';
 import { styles } from '../theme/appTheme';
+import { useCalculadora } from '../hooks/useCalculadora';
 
-enum Operadores {
-    sumar, restar, multiplicar, dividir
-}
 
 export const CalculadoraScreen = () => {
 
-  const [ numeroAnterior, setNumeroAnterior ] = useState('0');
-  const [ numero, setNumero ] = useState('0');
-
-  const ultimaOperacion = useRef<Operadores>();
-
-  const limpiar = () => {
-    setNumero('0');
-    setNumeroAnterior('0');
-  };
-
-  const armarNumero = ( numeroTexto: string ) => {
-
-    if ( numero.includes('.') && numeroTexto === '.' ) return;
-
-    if ( numero.startsWith('0') || numero.startsWith('-0') ) {
-
-      if ( numeroTexto === '.' ) {
-        setNumero( numero + numeroTexto );
-      } else if ( numeroTexto === '0' && numero.includes('.') ) {
-        setNumero( numero + numeroTexto );
-      } else if ( numeroTexto !== '0' && !numero.includes('.') ) {
-        setNumero( numeroTexto );
-      } else if ( numeroTexto === '0' && !numero.includes('.') ) {
-        setNumero(numero);
-      } else {
-        setNumero( numero + numeroTexto );
-      }
-
-    } else {
-      setNumero( numero + numeroTexto );
-    }
-
-  };
-
-  const positivoNegativo = () => {
-    if ( numero.includes('-') ) {
-      setNumero( numero.replace('-', ''));
-    } else {
-      setNumero('-' + numero );
-    }
-  };
-
-  const btnDelete = () => {
-
-    let negativo = '';
-    let numeroTemp = numero;
-    if ( numero.includes('-')) {
-      negativo = '-';
-      numeroTemp = numero.substring(1);
-    }
-
-    if ( numeroTemp.length > 1 ) {
-      setNumero(negativo + numeroTemp.slice(0, -1));
-    } else {
-      setNumero('0');
-    }
-  };
-
-  const cambiarNumPorAnterior = () => {
-    if ( numero.endsWith('.') ) {
-      setNumeroAnterior( numero.slice(0, -1) );
-    } else {
-      setNumeroAnterior( numero );
-    }
-    setNumero('0');
-  };
-
-  const btnDividir = () => {
-    cambiarNumPorAnterior();
-    ultimaOperacion.current = Operadores.dividir;
-  };
-
-  const btnMultiplicar = () => {
-    cambiarNumPorAnterior();
-    ultimaOperacion.current = Operadores.multiplicar;
-  };
-
-  const btnRestar = () => {
-    cambiarNumPorAnterior();
-    ultimaOperacion.current = Operadores.restar;
-  };
-
-  const btnSumar = () => {
-    cambiarNumPorAnterior();
-    ultimaOperacion.current = Operadores.sumar;
-  };
-
-  const calcular = () => {
-
-    const num1 = Number( numero );
-    const num2 = Number( numeroAnterior );
-
-    switch (ultimaOperacion.current) {
-      case Operadores.sumar:
-        setNumero(`${ num1 + num2 }`);
-        break;
-
-      case Operadores.restar:
-        setNumero(`${ num2 - num1 }`);
-        break;
-
-      case Operadores.multiplicar:
-        setNumero(`${ num1 * num2 }`);
-        break;
-
-      case Operadores.dividir:
-        setNumero(`${ num2 / num1 }`);
-        break;
-    }
-
-    setNumeroAnterior('0');
-  };
+  const {
+    numeroAnterior,
+    numero,
+    limpiar,
+    positivoNegativo,
+    btnDelete,
+    btnDividir,
+    armarNumero,
+    btnMultiplicar,
+    btnRestar,
+    btnSumar,
+    calcular,
+  } = useCalculadora();
 
   return (
     <View style={styles.calculadoraContainer}>
