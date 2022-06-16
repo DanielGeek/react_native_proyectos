d433bedded83db2c5dd66d8a073c905a6520b1ca
daniel-barreto <daniel30081990@gmail.com>
2022-06-15 21:58:29 -0400

    101. Start of project - NavigationApp

---
+++ 19-navegacionApp/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>App Screen</Text>
+    </View>
+  );
+};
+
+export default App;
