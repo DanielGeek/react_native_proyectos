3370549459f54426ba11eab4908c0dc1791433b3
daniel-barreto <daniel30081990@gmail.com>
2022-06-24 23:26:18 -0400

    123. Prepare the space for the icon

--- 19-navegacionApp/src/navigator/Tabs.tsx
+++ 19-navegacionApp/src/navigator/Tabs.tsx
@@ -6,7 +6,7 @@ import { Tab2Screen } from '../screens/Tab2Screen';
 import { StackNavigator } from './StackNavigator';
 import { colores } from '../theme/appTheme';
 import { BlurView } from 'expo-blur';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,18 +16,40 @@ export const Tabs = () => {
       sceneContainerStyle={{
         backgroundColor: 'white',
       }}
-      screenOptions={{
+      screenOptions={({ route }) => ({
         tabBarActiveTintColor: colores.primary,
         tabBarStyle: {
           borderTopColor: colores.primary,
           borderTopWidth: 0,
           elevation: 0,
         },
+        tabBarLabelStyle: {
+          fontSize: 15,
+        },
         tabBarBackground: () => (
           <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />
         ),
-      }}
+        tabBarIcon: ({ color, focused, size }) => {
+
+          let iconName: string = '';
+          switch (route.name) {
+            case 'Tab1Screen':
+              iconName = 'T1';
+              break;
+
+            case 'Tab2Screen':
+              iconName = 'T2';
+              break;
+
+            case 'StackNavigator':
+              iconName = 'St';
+              break;
+            }
+          return <Text style={{ color }}>{ iconName }</Text>;
+        },
+      })}
     >
+      {/* <Tab.Screen name="Tab1Screen" options={{title: 'Tab1', tabBarIcon: (props) => <Text style={{ color: props.color}}>T1</Text>}} component={Tab1Screen} /> */}
       <Tab.Screen name="Tab1Screen" options={{title: 'Tab1'}} component={Tab1Screen} />
       <Tab.Screen name="Tab2Screen" options={{title: 'Tab2'}} component={Tab2Screen} />
       <Tab.Screen name="StackNavigator" options={{title: 'Stack'}} component={StackNavigator} />
