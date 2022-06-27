0bef5954e39c1584a59e184e4aba40359d6a7660
daniel-barreto <daniel30081990@gmail.com>
2022-06-26 20:49:54 -0400

    124. Material Bottom Tab Navigator

--- 19-navegacionApp/src/navigator/Tabs.tsx
+++ 19-navegacionApp/src/navigator/Tabs.tsx
@@ -1,18 +1,79 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
 import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 import { StackNavigator } from './StackNavigator';
 import { colores } from '../theme/appTheme';
 import { BlurView } from 'expo-blur';
-import { StyleSheet, Text } from 'react-native';
+import { Platform, StyleSheet, Text } from 'react-native';
 
-const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
+  return Platform.OS === 'ios'
+          ? <TabsIOS />
+          : <TabsAndroid />;
+};
+
+
+
+const BottomTabAndroid = createMaterialBottomTabNavigator();
+
+const TabsAndroid = () => {
   return (
-    <Tab.Navigator
+    <BottomTabAndroid.Navigator
+      sceneAnimationEnabled={ true }
+      barStyle={{
+        backgroundColor: colores.primary,
+      }}
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: colores.primary,
+        tabBarStyle: {
+          borderTopColor: colores.primary,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 15,
+        },
+        tabBarBackground: () => (
+          <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />
+        ),
+        tabBarIcon: ({ color, focused }) => {
+
+          let iconName: string = '';
+          switch (route.name) {
+            case 'Tab1Screen':
+              iconName = 'T1';
+              break;
+
+            case 'Tab2Screen':
+              iconName = 'T2';
+              break;
+
+            case 'StackNavigator':
+              iconName = 'St';
+              break;
+            }
+          return <Text style={{ color }}>{ iconName }</Text>;
+        },
+      })}
+    >
+      <BottomTabAndroid.Screen name="Tab1Screen" options={{title: 'Tab1'}} component={Tab1Screen} />
+      <BottomTabAndroid.Screen name="Tab2Screen" options={{title: 'Tab2'}} component={Tab2Screen} />
+      <BottomTabAndroid.Screen name="StackNavigator" options={{title: 'Stack'}} component={StackNavigator} />
+    </BottomTabAndroid.Navigator>
+  );
+}
+
+
+const BottomTabIOS = createBottomTabNavigator();
+
+const TabsIOS = () => {
+  return (
+    <BottomTabIOS.Navigator
       sceneContainerStyle={{
         backgroundColor: 'white',
       }}
@@ -50,9 +111,9 @@ export const Tabs = () => {
       })}
     >
       {/* <Tab.Screen name="Tab1Screen" options={{title: 'Tab1', tabBarIcon: (props) => <Text style={{ color: props.color}}>T1</Text>}} component={Tab1Screen} /> */}
-      <Tab.Screen name="Tab1Screen" options={{title: 'Tab1'}} component={Tab1Screen} />
-      <Tab.Screen name="Tab2Screen" options={{title: 'Tab2'}} component={Tab2Screen} />
-      <Tab.Screen name="StackNavigator" options={{title: 'Stack'}} component={StackNavigator} />
-    </Tab.Navigator>
+      <BottomTabIOS.Screen name="Tab1Screen" options={{title: 'Tab1'}} component={Tab1Screen} />
+      <BottomTabIOS.Screen name="Tab2Screen" options={{title: 'Tab2'}} component={Tab2Screen} />
+      <BottomTabIOS.Screen name="StackNavigator" options={{title: 'Stack'}} component={StackNavigator} />
+    </BottomTabIOS.Navigator>
   );
 };
