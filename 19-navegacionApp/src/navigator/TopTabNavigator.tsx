a505f4f36fa640e9a7df214abe4b05b6c4858bad
daniel-barreto <daniel30081990@gmail.com>
2022-06-28 20:21:47 -0400

    126. Customizing the Material Top Tab Navigator

--- 19-navegacionApp/src/navigator/TopTabNavigator.tsx
+++ 19-navegacionApp/src/navigator/TopTabNavigator.tsx
@@ -1,15 +1,60 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { ChatScreen } from '../screens/ChatScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
 import { AlbumsScreen } from '../screens/AlbumsScreen';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { colores } from '../theme/appTheme';
+import { BlurView } from 'expo-blur';
+import { StyleSheet, Text } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabNavigator = () => {
+
+  const { top: paddingTop } = useSafeAreaInsets();
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      style={{ paddingTop }}
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      screenOptions={({ route }) => ({
+        tabBarPressColor: colores.primary,
+        tabBarShowIcon: true,
+        tabBarIndicatorStyle:{
+          backgroundColor: colores.primary,
+        },
+        tabBarStyle: {
+          shadowColor: 'transparent',
+          elevation: 0,
+        },
+        tabBarBackground: () => (
+          <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />
+        ),
+        tabBarIcon: ({ color, focused }) => {
+
+          let iconName: string = '';
+          switch (route.name) {
+            case 'Chat':
+              iconName = 'Ch';
+              break;
+
+            case 'Contacts':
+              iconName = 'Co';
+              break;
+
+            case 'Albums':
+              iconName = 'Al';
+              break;
+            }
+          return <Text style={{ color }}>{ iconName }</Text>;
+        },
+      })}
+    >
       <Tab.Screen name="Chat" component={ChatScreen} />
       <Tab.Screen name="Contacts" component={ContactsScreen} />
       <Tab.Screen name="Albums" component={AlbumsScreen} />
