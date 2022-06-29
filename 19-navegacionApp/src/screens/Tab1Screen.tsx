69cbc026a16a6ed61c9ca150e96e1edf951f5ed2
daniel-barreto <daniel30081990@gmail.com>
2022-06-28 21:37:06 -0400

    add more icons

--- 19-navegacionApp/src/screens/Tab1Screen.tsx
+++ 19-navegacionApp/src/screens/Tab1Screen.tsx
@@ -2,19 +2,31 @@ import React, { useEffect } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { Text, View } from 'react-native';
-import { styles } from '../theme/appTheme';
+import { colores, styles } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const Tab1Screen = () => {
+
+  const { top } = useSafeAreaInsets();
 
   useEffect(() => {
     console.log('Tab1Screen effect');
   }, []);
 
   return (
-    <View style={ styles.globalMargin }>
+    <View style={{
+          ...styles.globalMargin,
+          marginTop: top + 10,
+      }}>
       <Text style={ styles.title } >Iconos</Text>
       <Text>
-        <Icon name="airplane-outline" size={80} color="#900" />
+        <Icon name="airplane-outline" size={80} color={colores.primary} />
+        <Icon name="add-outline" size={80} color={colores.primary} />
+        <Icon name="aperture-outline" size={80} color={colores.primary} />
+        <Icon name="barbell-outline" size={80} color={colores.primary} />
+        <Icon name="bulb-outline" size={80} color={colores.primary} />
+        <Icon name="cloud-outline" size={80} color={colores.primary} />
+        <Icon name="happy-outline" size={80} color={colores.primary} />
       </Text>
     </View>
   );
