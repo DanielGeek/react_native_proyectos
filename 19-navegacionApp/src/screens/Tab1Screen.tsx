6825f5fad853d38dcafd895b1e74113eccb00ef1
daniel-barreto <daniel30081990@gmail.com>
2022-06-28 21:25:00 -0400

    127. Icons in our app - Facilities

--- 19-navegacionApp/src/screens/Tab1Screen.tsx
+++ 19-navegacionApp/src/screens/Tab1Screen.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import { Text, View } from 'react-native';
+import { styles } from '../theme/appTheme';
 
 export const Tab1Screen = () => {
 
@@ -8,8 +11,11 @@ export const Tab1Screen = () => {
   }, []);
 
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={ styles.globalMargin }>
+      <Text style={ styles.title } >Iconos</Text>
+      <Text>
+        <Icon name="airplane-outline" size={80} color="#900" />
+      </Text>
     </View>
   );
 };
