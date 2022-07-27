f948cb175eaf3632783021319708f50917805bd4
daniel-barreto <daniel30081990@gmail.com>
2022-07-26 20:18:11 -0400

    138. Reducer - useReducer

--- 19-navegacionApp/src/context/AuthContext.tsx
+++ 19-navegacionApp/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+import { authReducer } from './authReducer';
 
 export interface AuthState {
   isLoggedIn: boolean;
@@ -22,9 +23,11 @@ export const AuthContext = createContext({} as AuthContextProps);
 
 export const AuthProvider = ({ children }: any) => {
 
+  const [authState, dispatch] = useReducer( authReducer, authInitialState );
+
   return (
     <AuthContext.Provider value={{
-      authState: authInitialState,
+      authState,
       signIn: () => {},
     }}>
       { children }
---
+++ 19-navegacionApp/src/context/authReducer.tsx
@@ -0,0 +1,5 @@
+import { AuthState } from './AuthContext';
+
+export const authReducer = ( state: AuthState, action: any ): AuthState => {
+
+}
