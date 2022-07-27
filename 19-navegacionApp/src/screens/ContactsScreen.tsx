07e4aa41c0f9400ae43c14c11d7c2e49ddff7521
daniel-barreto <daniel30081990@gmail.com>
2022-07-26 20:43:43 -0400

    140. Trigger SignIn from another screen

--- 19-navegacionApp/src/screens/ContactsScreen.tsx
+++ 19-navegacionApp/src/screens/ContactsScreen.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Button } from 'react-native';
+import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 export const ContactsScreen = () => {
+
+  const { signIn, authState } = useContext( AuthContext );
+  const { isLoggedIn } = authState;
+
   return (
-    <View>
-      <Text>ContactsScreen</Text>
+    <View style={ styles.globalMargin }>
+      <Text style={ styles.title }>ContactsScreen</Text>
+
+      {
+        !isLoggedIn && <Button title="SignIn" onPress={ signIn }/>
+      }
+
     </View>
   );
 };
