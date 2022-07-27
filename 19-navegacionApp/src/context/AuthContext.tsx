2a97da1801b6d28cbd3f964cc3bcbd9aa57a232c
daniel-barreto <daniel30081990@gmail.com>
2022-07-26 20:30:45 -0400

    139. Shoot Actions

--- 19-navegacionApp/src/context/AuthContext.tsx
+++ 19-navegacionApp/src/context/AuthContext.tsx
@@ -25,10 +25,12 @@ export const AuthProvider = ({ children }: any) => {
 
   const [authState, dispatch] = useReducer( authReducer, authInitialState );
 
+  const signIn = () => {dispatch({ type: 'signIn' });};
+
   return (
     <AuthContext.Provider value={{
       authState,
-      signIn: () => {},
+      signIn,
     }}>
       { children }
     </AuthContext.Provider>
--- 19-navegacionApp/src/context/authReducer.tsx
+++ 19-navegacionApp/src/context/authReducer.tsx
@@ -1,5 +1,18 @@
 import { AuthState } from './AuthContext';
 
-export const authReducer = ( state: AuthState, action: any ): AuthState => {
+type AuthAction = { type: 'signIn' };
 
+export const authReducer = ( state: AuthState, action: AuthAction ): AuthState => {
+
+  switch (action.type) {
+    case 'signIn':
+      return {
+        ...state,
+        isLoggedIn: true,
+        username: 'no-username-yet',
+      };
+
+    default:
+      return state;
+  }
 }
