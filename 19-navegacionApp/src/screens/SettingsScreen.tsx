96482fb4e1a097ad14ddbc3e1cf58589d7e7e9b1
daniel-barreto <daniel30081990@gmail.com>
2022-07-26 20:08:38 -0400

    137. Consume the context - AuthState

--- 19-navegacionApp/src/screens/SettingsScreen.tsx
+++ 19-navegacionApp/src/screens/SettingsScreen.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 export const SettingsScreen = () => {
 
   const insets = useSafeAreaInsets();
+
+  const { authState } = useContext(AuthContext);
 
   return (
     <View style={{
           ...styles.globalMargin,
           marginTop: insets.top + 20,
       }}>
-      <Text style={ styles.title }>SettingsScreen</Text>
+      <Text style={ styles.title }>Settings Screen</Text>
+      <Text>{ JSON.stringify(authState, null, 4) }</Text>
     </View>
   );
 };
