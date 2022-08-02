194826bb8b50d9846de15c5d46b21b40cb9a67c2
daniel-barreto <daniel30081990@gmail.com>
2022-08-02 10:24:55 -0400

    155. Card Carousel

--- 20-PeliculasApp-2022/src/screens/HomeScreen.tsx
+++ 20-PeliculasApp-2022/src/screens/HomeScreen.tsx
@@ -1,9 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
-import { useMovies } from '../hooks/useMovies';
-import { MoviePoster } from '../components/MoviePoster';
+import { ActivityIndicator, Dimensions, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Carousel from 'react-native-snap-carousel';
+
+import { MoviePoster } from '../components/MoviePoster';
+import { useMovies } from '../hooks/useMovies';
+
+const { width: windowWidth } = Dimensions.get('window');
 
 export const HomeScreen = () => {
 
@@ -20,9 +24,14 @@ export const HomeScreen = () => {
 
   return (
     <View style={{ marginTop: top + 20 }}>
-      <MoviePoster
-        movie={ peliculasEnCine[0] }
-      />
+      <View style={{ height: 440 }}>
+        <Carousel
+            data={ peliculasEnCine }
+            renderItem={ ({ item }: any ) => <MoviePoster movie={ item } />}
+            sliderWidth={ windowWidth }
+            itemWidth={ 300 }
+        />
+      </View>
     </View>
   );
 };
