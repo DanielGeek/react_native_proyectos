0de829f630d75cc0069bf269b8260dec5a82fb53
daniel-barreto <daniel30081990@gmail.com>
2022-08-02 08:26:30 -0400

    152. Typing of TheMovieDB

--- 20-PeliculasApp-2022/src/screens/HomeScreen.tsx
+++ 20-PeliculasApp-2022/src/screens/HomeScreen.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 import movieDB from '../api/movieDB';
+import { MovieDBNowPlaying } from '../interfaces/movieInterface';
 
 export const HomeScreen = () => {
 
   useEffect(() => {
-    movieDB.get('/now_playing')
+    movieDB.get<MovieDBNowPlaying>('/now_playing')
       .then( resp => {
-        console.log(resp.data);
+        console.log(resp.data.results[0].title);
       });
   }, []);
 
