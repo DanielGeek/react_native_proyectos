93424596463e8158408271ae58db8c6fa4465602
daniel-barreto <daniel30081990@gmail.com>
2022-08-03 22:45:10 -0400

    167. Component to display actors

---
+++ 20-PeliculasApp-2022/src/components/CastItem.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Cast } from '../interfaces/creditsInterface';
+
+interface Props {
+  actor: Cast
+}
+
+export const CastItem = ({ actor }: Props) => {
+
+  const uri = `https://image.tmdb.org/t/p/w500/${ actor.profile_path }`;
+  return (
+    <View style={ styles.container }>
+      <Image
+        source={{ uri }}
+        style={{ width: 50, height: 50, borderRadius: 10 }}
+      />
+      <View style={ styles.actorInfo }>
+        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+          { actor.name }
+        </Text>
+        <Text style={{ fontSize: 16, opacity: 0.7 }}>
+          { actor.character }
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      backgroundColor: 'white',
+      borderRadius: 10,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 10,
+      },
+      shadowOpacity: 0.24,
+      shadowRadius: 7,
+
+      elevation: 9,
+    },
+    actorInfo: {
+      marginLeft: 20,
+    },
+});
--- 20-PeliculasApp-2022/src/components/MovieDetails.tsx
+++ 20-PeliculasApp-2022/src/components/MovieDetails.tsx
@@ -6,6 +6,7 @@ import currencyFormatter from 'currency-formatter';
 import { MovieFull } from '../interfaces/movieInterface';
 import { Cast } from '../interfaces/creditsInterface';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { CastItem } from './CastItem';
 
 interface Props {
   movieFull: MovieFull;
@@ -15,7 +16,7 @@ interface Props {
 
 export const MovieDetails = ({ movieFull, cast }: Props) => {
   return (
-    <View>
+    <>
       {/* Detalles */}
       <View style={{ marginHorizontal: 20 }}>
         <View style={{ flexDirection: 'row'}}>
@@ -44,11 +45,19 @@ export const MovieDetails = ({ movieFull, cast }: Props) => {
         <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold' }}>
           Presupuesto
         </Text>
-        <Text style={{ fontSize: 18 }}>{ currencyFormatter.format(movieFull.budget, { code:'USD' }) }</Text>
+        <Text style={{ fontSize: 18 }}>
+          { currencyFormatter.format(movieFull.budget, { code:'USD' }) }
+        </Text>
 
       </View>
 
       {/* Casting */}
-    </View>
+      <View style={{ marginTop: 10, marginBottom: 100 }}>
+        <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold', marginHorizontal: 20 }}>
+          Actores
+        </Text>
+        <CastItem actor={ cast[0] } />
+      </View>
+    </>
   );
 };
