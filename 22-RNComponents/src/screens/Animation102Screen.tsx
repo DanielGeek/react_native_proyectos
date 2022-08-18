dbab1473554a87fc02e603f3651cfdadc54f25cc
daniel-barreto <daniel30081990@gmail.com>
2022-08-18 18:43:40 -0400

    198. Animated ValueXY

--- 22-RNComponents/src/screens/Animation102Screen.tsx
+++ 22-RNComponents/src/screens/Animation102Screen.tsx
@@ -1,21 +1,48 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, View, Animated, PanResponder } from 'react-native';
+import { useRef } from 'react';
 
 export const Animation102Screen = () => {
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        { dx: pan.x, dy: pan.y },
+      ],{
+        useNativeDriver: false,
+      }),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed
+        { toValue: { x: 0, y: 0 }, useNativeDriver: false } // Back to zero
+      ).start();
+    },
+  });
+
   return (
-    <View style={{ flex: 1 }}>
-      <View style={ styles.purpleBox } />
+    <View style={ styles.container }>
+      <Animated.View
+        { ...panResponder.panHandlers }
+        style={[pan.getLayout(), styles.purpleBox]}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
-    purpleBox: {
-      backgroundColor: 'red',
-      width: 150,
-      height: 150,
-    },
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  purpleBox: {
+    backgroundColor: '#75CEDB',
+    width: 150,
+    height: 150,
+  },
 });
