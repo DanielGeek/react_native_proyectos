9d766817820a1415d4277bc59e58d3ca42cf94f3
daniel-barreto <daniel30081990@gmail.com>
2022-08-18 09:46:39 -0400

    190. Component - FlatList

--- 22-RNComponents/src/screens/HomeScreen.tsx
+++ 22-RNComponents/src/screens/HomeScreen.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+interface MenuItem {
+  name: String;
+  icon: String;
+  components: String;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    components: 'Animation101Screen',
+  },
+];
 
 export const HomeScreen = () => {
+
+  const renderMenuItem = ( menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>{ menuItem.name } - {menuItem.icon}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Icon
-        name="star-outline"
-        size={ 50 }
-        color="black"
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={ menuItems }
+        renderItem={ ({ item }) => renderMenuItem( item ) }
+        keyExtractor={ ( item ) => item.name }
       />
     </View>
   );
