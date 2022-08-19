1deb743f29d436aa2dfcb5f0194af63baedd15a7
daniel-barreto <daniel30081990@gmail.com>
2022-08-19 12:42:03 -0400

    207. Multiple TextInputs on screen

--- 22-RNComponents/src/screens/TextInputScreen.tsx
+++ 22-RNComponents/src/screens/TextInputScreen.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
 export const TextInputScreen = () => {
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
+  const onChange = ( value: string, field: string ) => {
+    setForm({
+      ...form,
+      [field]: value,
+    });
+  };
+
   return (
     <View style={ styles.globalMargin }>
       <HeaderTitle title="TextInputs" />
 
       <TextInput
         style={ stylesScreen.inputStyle }
+        placeholder="Ingrese su nombre"
+        autoCorrect={ false }
+        autoCapitalize="words"
+        onChangeText={ ( value ) => onChange( value, 'name')}
       />
+      <TextInput
+        style={ stylesScreen.inputStyle }
+        placeholder="Ingrese su email"
+        autoCorrect={ false }
+        autoCapitalize="none"
+        onChangeText={ ( value ) => onChange( value, 'email')}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={ stylesScreen.inputStyle }
+        placeholder="Ingrese su teléfono"
+        onChangeText={ ( value ) => onChange( value, 'phone')}
+        keyboardType="phone-pad"
+      />
+
+      <HeaderTitle title={ JSON.stringify( form, null, 3 )} />
     </View>
   );
 };
@@ -22,5 +56,6 @@ const stylesScreen = StyleSheet.create({
       height: 50,
       paddingHorizontal: 10,
       borderRadius: 10,
+      marginVertical: 10,
     },
 });
