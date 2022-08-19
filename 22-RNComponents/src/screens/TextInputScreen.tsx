64657483cfc8da6187a974491fb073c6137b39d8
daniel-barreto <daniel30081990@gmail.com>
2022-08-19 13:23:20 -0400

    208. Component - keyboardavoidingview

--- 22-RNComponents/src/screens/TextInputScreen.tsx
+++ 22-RNComponents/src/screens/TextInputScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View, KeyboardAvoidingView, Platform, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
@@ -19,33 +19,46 @@ export const TextInputScreen = () => {
   };
 
   return (
-    <View style={ styles.globalMargin }>
-      <HeaderTitle title="TextInputs" />
+    <KeyboardAvoidingView
+      behavior={ Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={ styles.globalMargin }>
+            <HeaderTitle title="TextInputs" />
 
-      <TextInput
-        style={ stylesScreen.inputStyle }
-        placeholder="Ingrese su nombre"
-        autoCorrect={ false }
-        autoCapitalize="words"
-        onChangeText={ ( value ) => onChange( value, 'name')}
-      />
-      <TextInput
-        style={ stylesScreen.inputStyle }
-        placeholder="Ingrese su email"
-        autoCorrect={ false }
-        autoCapitalize="none"
-        onChangeText={ ( value ) => onChange( value, 'email')}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={ stylesScreen.inputStyle }
-        placeholder="Ingrese su teléfono"
-        onChangeText={ ( value ) => onChange( value, 'phone')}
-        keyboardType="phone-pad"
-      />
+            <TextInput
+              style={ stylesScreen.inputStyle }
+              placeholder="Ingrese su nombre"
+              autoCorrect={ false }
+              autoCapitalize="words"
+              onChangeText={ ( value ) => onChange( value, 'name')}
+            />
+            <TextInput
+              style={ stylesScreen.inputStyle }
+              placeholder="Ingrese su email"
+              autoCorrect={ false }
+              autoCapitalize="none"
+              onChangeText={ ( value ) => onChange( value, 'email')}
+              keyboardType="email-address"
+              keyboardAppearance="dark"
+            />
 
-      <HeaderTitle title={ JSON.stringify( form, null, 3 )} />
-    </View>
+            <HeaderTitle title={ JSON.stringify( form, null, 3 )} />
+            <HeaderTitle title={ JSON.stringify( form, null, 3 )} />
+
+            <TextInput
+              style={ stylesScreen.inputStyle }
+              placeholder="Ingrese su teléfono"
+              onChangeText={ ( value ) => onChange( value, 'phone')}
+              keyboardType="phone-pad"
+            />
+            <View style={{ height: 100 }} />
+          </View>
+
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
