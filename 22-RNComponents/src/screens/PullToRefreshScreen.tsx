2f412764bced76702e903ae9a7c2f9ca9d490efb
daniel-barreto <daniel30081990@gmail.com>
2022-08-20 21:05:55 -0400

    211. Customizing the Refresh Control

--- 22-RNComponents/src/screens/PullToRefreshScreen.tsx
+++ 22-RNComponents/src/screens/PullToRefreshScreen.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View, ScrollView, RefreshControl } from 'react-native';
+import { View, ScrollView, RefreshControl, SafeAreaView } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 import { useState } from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const PullToRefreshScreen = () => {
-
+  const { top } = useSafeAreaInsets();
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState<string>();
 
   const onRefresh = () => {
     setRefreshing(true);
@@ -14,20 +17,30 @@ export const PullToRefreshScreen = () => {
     setTimeout(() => {
       console.log('Terminamos');
       setRefreshing(false);
+      setData('Hola mundo');
     }, 1500);
   };
 
   return (
     <ScrollView
+      style={{
+        marginTop: refreshing ? top : 0,
+      }}
       refreshControl={
         <RefreshControl
           refreshing={refreshing}
           onRefresh={ onRefresh }
+          progressViewOffset={10}
+          progressBackgroundColor="#5856D6"
+          colors={[ 'white', 'red', 'orange' ]}
+          style={{ backgroundColor: '#5856D6' }}
+          tintColor="white"
         />
       }
     >
       <View style={ styles.globalMargin }>
         <HeaderTitle title="Pull to refresh" />
+        { data && <HeaderTitle title={ data } /> }
       </View>
     </ScrollView>
   );
