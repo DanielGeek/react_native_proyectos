075de997969cff9407717cfb6e4ef45da5ec71cf
daniel-barreto <daniel30081990@gmail.com>
2022-08-21 18:18:51 -0400

    217. InfiniteScroll with images

--- 22-RNComponents/src/screens/InfiniteScrollScreen.tsx
+++ 22-RNComponents/src/screens/InfiniteScrollScreen.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { useState } from 'react';
 
@@ -12,14 +13,21 @@ export const InfiniteScrollScreen = () => {
       newArray[i] = numbers.length + 1;
     }
 
-    setNumbers([...numbers, ...newArray]);
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 1500);
+
   };
 
   const renderItem = ( item: number ) => {
     return (
-      <Text style={ styles.textItem }>
-        { item }
-      </Text>
+      <Image
+        source={{ uri: `https://picsum.photos/id/${ item }/500/400`}}
+        style={{
+          width: '100%',
+          height: 400,
+        }}
+      />
     );
   };
 
@@ -34,6 +42,17 @@ export const InfiniteScrollScreen = () => {
 
         onEndReached={ loadMore }
         onEndReachedThreshold={ 0.5 }
+
+        ListFooterComponent={() => (
+          <View style={{
+            height: 150,
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <ActivityIndicator size={25} color="#5856D6" />
+          </View>
+        )}
       />
     </View>
   );
