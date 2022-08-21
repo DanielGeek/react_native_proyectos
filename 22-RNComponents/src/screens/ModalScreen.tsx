0a6820b9742b65eec392106cab9def49d1c1c99b
daniel-barreto <daniel30081990@gmail.com>
2022-08-21 15:03:38 -0400

    215. Customize the modal

--- 22-RNComponents/src/screens/ModalScreen.tsx
+++ 22-RNComponents/src/screens/ModalScreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { Button, Modal, Text, View } from 'react-native';
 import { HeaderTitle } from '../components/HeaderTitle';
@@ -23,14 +24,27 @@ export const ModalScreen = () => {
       >
         <View style={{
           flex: 1,
-          backgroundColor: 'rgba(0,0,0,0.3)'
+          backgroundColor: 'rgba(0,0,0,0.3)',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}>
 
           <View style={{
-            backgroundColor: 'white'
+            width: 200,
+            height: 200,
+            backgroundColor: 'white',
+            justifyContent: 'center',
+            alignItems: 'center',
+            shadowOffset: {
+              width: 0,
+              height: 10,
+            },
+            shadowOpacity: 0.25,
+            elevation: 10,
+            borderRadius: 5,
           }}>
-            <HeaderTitle title="Modal" />
-            <Text>Cuerpo del modal</Text>
+            <Text style={{ fontSize: 20, fontWeight: 'bold'}}>Modal</Text>
+            <Text style={{ fontSize: 16, fontWeight: '300', marginBottom: 20 }}>Cuerpo del modal</Text>
             <Button
               title="Cerrar"
               onPress={ () => setIsVisible(false)}
