4daf1ee4339594a4cebbf4e1faf6630b6f3b2b3f
daniel-barreto <daniel30081990@gmail.com>
2022-08-22 19:21:25 -0400

    228. Button on last slide

--- 22-RNComponents/src/components/SlidesScreen.tsx
+++ 22-RNComponents/src/components/SlidesScreen.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View, SafeAreaView, ImageSourcePropType, Text, Dimensions, Image, StyleSheet } from 'react-native';
+import { View, SafeAreaView, ImageSourcePropType, Text, Dimensions, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import { useState } from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const { height: screenHeight, width:screenWidth } = Dimensions.get('window');
 
@@ -75,17 +76,46 @@ export const SlidesScreen = () => {
           setActiveIndex(index);
         }}
       />
-
-      <Pagination
-        dotsLength={ items.length }
-        activeDotIndex={ activeIndex }
-        dotStyle={{
-          width: 10,
-          height: 10,
-          borderRadius: 5,
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 20,
+        alignItems: 'center',
+      }}>
+        <Pagination
+          dotsLength={ items.length }
+          activeDotIndex={ activeIndex }
+          dotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 5,
+            backgroundColor: '#5856D6',
+          }}
+        />
+        <TouchableOpacity style={{
+          flexDirection: 'row',
           backgroundColor: '#5856D6',
+          width: 140,
+          height: 50,
+          borderRadius: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
-      />
+          activeOpacity={ 0.8 }
+        >
+          <Text style={{
+            fontSize: 25,
+            color: 'white',
+          }}>
+            Entrar
+          </Text>
+          <Icon
+            name="chevron-forward-outline"
+            color="white"
+            size={30}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
