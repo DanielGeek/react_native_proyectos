a133c367abe6e76dc2fe3eae7a8d027472f52088
daniel-barreto <daniel30081990@gmail.com>
2022-08-23 15:20:58 -0400

    245. Basic HomeScreen Design

--- 23-Pokedex/src/navigator/Navigator.tsx
+++ 23-Pokedex/src/navigator/Navigator.tsx
@@ -7,7 +7,14 @@ const Stack = createStackNavigator();
 
 export const Navigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white',
+        },
+      }}
+    >
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
     </Stack.Navigator>
--- 23-Pokedex/src/screens/HomeScreen.tsx
+++ 23-Pokedex/src/screens/HomeScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { styles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
+
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      <Image
+        source={ require('../assets/pokebola.png')}
+        style={ styles.pokebolaBG }
+      />
+      <Text style={{
+          ...styles.title,
+          ...styles.globalMargin,
+          top: top + 20,
+      }}>Pokedex</Text>
+    </>
   );
 };
