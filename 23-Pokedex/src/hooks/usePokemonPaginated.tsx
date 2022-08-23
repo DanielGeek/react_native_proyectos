90eb919a14d7bdc0a469cfa9e5f2907d7afde1e6
daniel-barreto <daniel30081990@gmail.com>
2022-08-23 17:23:29 -0400

    247. Set the type of data returned by the HTTP request

--- 23-Pokedex/src/hooks/usePokemonPaginated.tsx
+++ 23-Pokedex/src/hooks/usePokemonPaginated.tsx
@@ -1,14 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from 'react';
 import { pokemonApi } from '../api/pokemonApi';
+
+import { PokemonPaginatedResponse, Result, SimplePokemon } from "../interfaces/pokemonInterfaces";
 
 export const usePokemonPaginated = () => {
 
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
   const loadPokemons = async() => {
 
-    const resp = await pokemonApi.get(nextPageUrl.current);
-    console.log(resp.data);
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(nextPageUrl.current);
+    nextPageUrl.current = resp.data.next;
+    mapPokemonList( resp.data.results );
+  };
+
+  const mapPokemonList = ( pokemonList: Result[] ) => {
+    pokemonList.forEach( poke => console.log(poke.name ));
   };
 
   useEffect(() => {
@@ -16,7 +24,7 @@ export const usePokemonPaginated = () => {
   }, []);
 
   return {
-    
+    simplePokemonList,
   };
 
 };
