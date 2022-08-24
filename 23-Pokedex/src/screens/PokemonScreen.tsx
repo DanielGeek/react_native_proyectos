61f26719a0d242706314bad663b65b70524048df
daniel-barreto <daniel30081990@gmail.com>
2022-08-23 22:57:19 -0400

    256. Pokemon Screen

--- 23-Pokedex/src/screens/PokemonScreen.tsx
+++ 23-Pokedex/src/screens/PokemonScreen.tsx
@@ -1,17 +1,91 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigator/Navigator';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FadeInImage } from '../components/FadeInImage';
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> {}
 
 export const PokemonScreen = ( { navigation, route }: Props ) => {
 
   const { simplePokemon, color } = route.params;
+  const { id, name, picture } = simplePokemon;
+  const { top } = useSafeAreaInsets();
 
   return (
     <View>
-      <Text style={{ color }}>{simplePokemon.name} - { color }</Text>
+      <View style={{
+        ...styles.headerContainer,
+        backgroundColor: color,
+      }}>
+        <TouchableOpacity
+              onPress={ () => navigation.pop() }
+              activeOpacity={0.8}
+              style={{
+                ...styles.backButton,
+                top: top + 5,
+              }}
+        >
+          <Icon
+            name="arrow-back-outline"
+            color="white"
+            size={ 35 }
+          />
+        </TouchableOpacity>
+
+        <Text
+          style={{
+            ...styles.pokemonName,
+            top: top + 45,
+          }}
+        >
+          { name + '\n' } #{ id }
+        </Text>
+
+        <Image
+          source={ require('../assets/pokebola-blanca.png')}
+          style={ styles.pokeball}
+        />
+
+        <FadeInImage
+          uri={ picture }
+          style={ styles.pokemonImage }
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    headerContainer: {
+      height: 370,
+      zIndex: 999,
+      alignItems: 'center',
+      borderBottomRightRadius: 1000,
+      borderBottomLeftRadius: 1000,
+    },
+    backButton: {
+      position: 'absolute',
+      left: 20,
+    },
+    pokemonName: {
+      color: 'white',
+      fontSize: 40,
+      alignSelf: 'flex-start',
+      left: 20,
+    },
+    pokeball: {
+      width: 250,
+      height: 250,
+      bottom: -20,
+      opacity: 0.7,
+    },
+    pokemonImage: {
+      width: 250,
+      height: 250,
+      position: 'absolute',
+      bottom: -15,
+    },
+});
