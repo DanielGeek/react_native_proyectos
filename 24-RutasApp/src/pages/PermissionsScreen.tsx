239c3898b35995fccc90abf572cc8d3826ce51da
daniel-barreto <daniel30081990@gmail.com>
2022-08-27 12:13:25 -0400

    288. Stylized Button

--- 24-RutasApp/src/pages/PermissionsScreen.tsx
+++ 24-RutasApp/src/pages/PermissionsScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { BlackButton } from '../components/BlackButton';
 import { PermissionsContext } from '../context/PermissionsContext';
 
 export const PermissionsScreen = () => {
@@ -8,14 +9,14 @@ export const PermissionsScreen = () => {
 
   return (
     <View style={ styles.container }>
-      <Text>PermissionsScreen</Text>
+      <Text style={ styles.title }>Es necesario el uso del GPS para usar esta aplicación</Text>
 
-      <Button
+      <BlackButton
           title="Permiso"
           onPress={ askLocationPermission }
       />
 
-      <Text>
+      <Text style={{ marginTop: 20 }}>
         { JSON.stringify(permissions, null, 5) }
       </Text>
     </View>
@@ -27,5 +28,11 @@ const styles = StyleSheet.create({
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
+    },
+    title: {
+      width: 250,
+      fontSize: 18,
+      textAlign: 'center',
+      marginBottom: 20,
     },
 });
