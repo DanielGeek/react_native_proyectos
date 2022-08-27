9b8ba0d1dfa32a1ae2339b9687670c1dfcac2bde
daniel-barreto <daniel30081990@gmail.com>
2022-08-26 22:37:41 -0400

    285. Check GPS permission when returning to the app

--- 24-RutasApp/src/context/PermissionsContext.tsx
+++ 24-RutasApp/src/context/PermissionsContext.tsx
@@ -1,6 +1,6 @@
-import React, { createContext, useState } from 'react';
-import { Platform } from 'react-native';
-import { PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
+import React, { createContext, useEffect, useState } from 'react';
+import { AppState, Platform } from 'react-native';
+import { check, PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
 
 
 export interface PermissionsState {
@@ -23,6 +23,18 @@ export const PermissionsProvider = ({ children }: any ) => {
 
   const [permissions, setPermissions] = useState(permissionInitState);
 
+  useEffect(() => {
+
+    AppState.addEventListener('change', state => {
+
+      if( state !== 'active' ) return;
+
+      checkLocationPermission();
+
+    });
+
+  }, []);
+
   const askLocationPermission = async() => {
 
     let permissionStatus: PermissionStatus;
@@ -44,7 +56,24 @@ export const PermissionsProvider = ({ children }: any ) => {
 
   };
 
-  const checkLocationPermission = () => {
+  const checkLocationPermission = async() => {
+
+    let permissionStatus: PermissionStatus;
+
+    if ( Platform.OS === 'ios' ) {
+
+      permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+
+    } else {
+
+      permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+
+    }
+
+    setPermissions({
+      ...permissions,
+      locationStatus: permissionStatus,
+    });
 
   };
 
