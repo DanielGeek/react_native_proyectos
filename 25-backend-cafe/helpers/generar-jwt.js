ef6c842767c9f6c2eb7fbdc9d56ea171022721aa
daniel-barreto <daniel30081990@gmail.com>
2022-08-28 19:35:54 -0400

    313. Downloads and basic configurations

---
+++ 25-backend-cafe/helpers/generar-jwt.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+
+
+const generarJWT = ( uid = '' ) => {
+
+    return new Promise( (resolve, reject) => {
+
+        const payload = { uid };
+
+        jwt.sign( payload, process.env.SECRETORPRIVATEKEY, {
+            expiresIn: '7d'
+        }, ( err, token ) => {
+
+            if ( err ) {
+                console.log(err);
+                reject( 'No se pudo generar el token' )
+            } else {
+                resolve( token );
+            }
+        })
+
+    })
+}
+
+
+
+
+module.exports = {
+    generarJWT
+}
+
