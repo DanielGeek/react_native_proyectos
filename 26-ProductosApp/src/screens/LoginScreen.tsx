793403bae1221a9f3962ac6857e4c9adb6c5c7a0
daniel-barreto <daniel30081990@gmail.com>
2022-08-29 00:22:08 -0400

    322. Login Design - Part 2

--- 26-ProductosApp/src/screens/LoginScreen.tsx
+++ 26-ProductosApp/src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { Platform, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { Background } from '../components/Background';
 import { WhiteLogo } from '../components/WhiteLogo';
 import { loginStyles } from '../theme/loginTheme';
@@ -9,19 +9,63 @@ export const LoginScreen = () => {
     <>
       {/* Background */}
       <Background />
+      <View style={ loginStyles.formContainer }>
+        {/* Keyboard avoid view */}
+        <WhiteLogo />
 
-      {/* Keyboard avoid view */}
-      <WhiteLogo />
+        <Text style={ loginStyles.title }>Login</Text>
 
-      <Text style={ loginStyles.title }>Login</Text>
+        <Text style={ loginStyles.label }>Email:</Text>
 
-      <Text style={ loginStyles.label }>Email:</Text>
+        <TextInput
+          placeholder="Ingrese su email:"
+          placeholderTextColor="rgba(255,255,255,0.4)"
+          keyboardType="email-address"
+          underlineColorAndroid="white"
+          style={[
+            loginStyles.inputField,
+            ( Platform.OS === 'ios' ) && loginStyles.inputFieldIOS,
+          ]}
+          selectionColor="white"
+          autoCapitalize="none"
+          autoCorrect={ false }
+        />
 
-      <TextInput
-        placeholder="Ingrese su email:"
-        placeholderTextColor="rgba(255,255,255,0.4)"
-        keyboardType='email-address'
-      />
+        <Text style={ loginStyles.label }>Password:</Text>
+
+        <TextInput
+          placeholder="******"
+          placeholderTextColor="rgba(255,255,255,0.4)"
+          underlineColorAndroid="white"
+          style={[
+            loginStyles.inputField,
+            ( Platform.OS === 'ios' ) && loginStyles.inputFieldIOS,
+          ]}
+          selectionColor="white"
+          autoCapitalize="none"
+          autoCorrect={ false }
+        />
+
+        {/* Boton login */}
+        <View style={ loginStyles.buttonContainer }>
+          <TouchableOpacity
+            activeOpacity={ 0.8 }
+            style={ loginStyles.button }
+          >
+            <Text style={ loginStyles.buttonText }>Login</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Crear una nueva cuenta */}
+        <View style={ loginStyles.newUserContainer }>
+          <TouchableOpacity
+            activeOpacity={ 0.8 }
+            onPress={ () => console.log('press') }
+          >
+            <Text style={loginStyles.buttonText}>Nueva cuenta </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 };
--- 26-ProductosApp/src/theme/loginTheme.tsx
+++ 26-ProductosApp/src/theme/loginTheme.tsx
@@ -1,7 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 export const loginStyles = StyleSheet.create({
-    title: {
+  formContainer: {
+    flex: 1,
+    paddingHorizontal: 30,
+    justifyContent: 'center',
+    height: 600,
+    marginBottom: 50,
+  },
+  title: {
       color: 'white',
       fontSize: 30,
       fontWeight: 'bold',
@@ -11,5 +18,33 @@ export const loginStyles = StyleSheet.create({
       marginTop: 25,
       color: 'white',
       fontWeight: 'bold',
+    },
+    inputField: {
+      color: 'white',
+      fontSize: 20,
+    },
+    inputFieldIOS: {
+      borderBottomColor: 'white',
+      borderBottomWidth: 2,
+      paddingBottom: 4,
+    },
+    buttonContainer: {
+      alignItems: 'center',
+      marginTop: 50,
+    },
+    button: {
+      borderWidth: 2,
+      borderColor: 'white',
+      paddingHorizontal: 20,
+      paddingVertical: 5,
+      borderRadius: 100,
+    },
+    buttonText: {
+      fontSize: 18,
+      color: 'white',
+    },
+    newUserContainer: {
+      alignItems: 'flex-end',
+      marginTop: 10,
     },
 });
